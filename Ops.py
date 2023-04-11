e3410f2186ad566e82eecc935921354c48efa2ac
Muhamad Hasbi <moyantcia@gmail.com>
2023-04-11 16:47:00 +0700

    remove psutil

--- Ops.py
+++ Ops.py
@@ -2,7 +2,6 @@ import os
 import glob
 import time
 import tqdm
-import psutil
 import logging
 from libs.Loggers import Loggers
 from settings.Config import Config
@@ -48,66 +47,41 @@ class Ops:
     def parse_message(self, data:dict) -> str:
         key_data = list(data.keys())
 
-        cpu_usage_0 = 0
-        memory_usage_0 = 0
-        network_usage_0 = 0
+        cpu_usage_0 = os.popen("ps aux | grep INews.py | grep -v grep | awk '{print $3}'").read()
+        memory_usage_0 = os.popen("ps aux | grep INews.py | grep -v grep | awk '{print $4}'").read()
+        network_usage_0 = os.popen("ps aux | grep INews.py | grep -v grep | awk '{print $6}'").read()
+        if cpu_usage_0 == "" : cpu_usage_0 = 0
+        if memory_usage_0 == "" : memory_usage_0 = 0
+        if network_usage_0 == "" : network_usage_0 = 0
 
-        cpu_usage_1 = 0
-        memory_usage_1 = 0
-        network_usage_1 = 0
+        cpu_usage_1 = os.popen("ps aux | grep CNNIndonesia.py | grep -v grep | awk '{print $3}'").read()
+        memory_usage_1 = os.popen("ps aux | grep CNNIndonesia.py | grep -v grep | awk '{print $4}'").read()
+        network_usage_1 = os.popen("ps aux | grep CNNIndonesia.py | grep -v grep | awk '{print $6}'").read()
+        if cpu_usage_1 == "" : cpu_usage_1 = 0
+        if memory_usage_1 == "" : memory_usage_1 = 0
+        if network_usage_1 == "" : network_usage_1 = 0
 
-        cpu_usage_2 = 0
-        memory_usage_2 = 0
-        network_usage_2 = 0
+        cpu_usage_2 = os.popen("ps aux | grep MetroTV.py | grep -v grep | awk '{print $3}'").read() 
+        memory_usage_2 = os.popen("ps aux | grep MetroTV.py | grep -v grep | awk '{print $4}'").read()
+        network_usage_2 = os.popen("ps aux | grep MetroTV.py | grep -v grep | awk '{print $6}'").read()
+        if cpu_usage_2 == "" : cpu_usage_2 = 0
+        if memory_usage_2 == "" : memory_usage_2 = 0
+        if network_usage_2 == "" : network_usage_2 = 0
 
-        cpu_usage_3 = 0
-        memory_usage_3 = 0
-        network_usage_3 = 0
+        cpu_usage_3 = os.popen("ps aux | grep KompasTV.py | grep -v grep | awk '{print $3}'").read()
+        memory_usage_3 = os.popen("ps aux | grep KompasTV.py | grep -v grep | awk '{print $4}'").read()
+        network_usage_3 = os.popen("ps aux | grep KompasTV.py | grep -v grep | awk '{print $6}'").read()
+        if cpu_usage_3 == "" : cpu_usage_3 = 0
+        if memory_usage_3 == "" : memory_usage_3 = 0
+        if network_usage_3 == "" : network_usage_3 = 0
 
-        cpu_usage_4 = 0
-        memory_usage_4 = 0
-        network_usage_4 = 0
-        
-        for proc in psutil.process_iter():
-            if "INews.py" in proc.name():
-                cpu_usage_0 = proc.cpu_percent()
-                memory_usage_0 = proc.memory_percent()
-                network_usage_0 = proc.io_counters().write_bytes / 1024 / 1024
-                if cpu_usage_0 == "" : cpu_usage_0 = 0
-                if memory_usage_0 == "" : memory_usage_0 = 0
-                if network_usage_0 == "" : network_usage_0 = 0
-            
-            if "CNNIndonesia.py" in proc.name():
-                cpu_usage_1 = proc.cpu_percent()
-                memory_usage_1 = proc.memory_percent()
-                network_usage_1 = proc.io_counters().write_bytes / 1024 / 1024
-                if cpu_usage_1 == "" : cpu_usage_1 = 0
-                if memory_usage_1 == "" : memory_usage_1 = 0
-                if network_usage_1 == "" : network_usage_1 = 0
+        cpu_usage_4 = os.popen("ps aux | grep ServerConverter.py | grep -v grep | awk '{print $3}'").read()
+        memory_usage_4 = os.popen("ps aux | grep ServerConverter.py | grep -v grep | awk '{print $4}'").read()
+        network_usage_4 = os.popen("ps aux | grep ServerConverter.py | grep -v grep | awk '{print $6}'").read()
+        if cpu_usage_4 == "" : cpu_usage_4 = 0
+        if memory_usage_4 == "" : memory_usage_4 = 0
+        if network_usage_4 == "" : network_usage_4 = 0
 
-            if "MetroTV.py" in proc.name():
-                cpu_usage_2 = proc.cpu_percent()
-                memory_usage_2 = proc.memory_percent()
-                network_usage_2 = proc.io_counters().write_bytes / 1024 / 1024
-                if cpu_usage_2 == "" : cpu_usage_2 = 0
-                if memory_usage_2 == "" : memory_usage_2 = 0
-                if network_usage_2 == "" : network_usage_2 = 0
-
-            if "KompasTV.py" in proc.name():
-                cpu_usage_3 = proc.cpu_percent()
-                memory_usage_3 = proc.memory_percent()
-                network_usage_3 = proc.io_counters().write_bytes / 1024 / 1024
-                if cpu_usage_3 == "" : cpu_usage_3 = 0
-                if memory_usage_3 == "" : memory_usage_3 = 0
-                if network_usage_3 == "" : network_usage_3 = 0
-
-            if "ServerConverter.py" in proc.name():
-                cpu_usage_4 = proc.cpu_percent()
-                memory_usage_4 = proc.memory_percent()
-                network_usage_4 = proc.io_counters().write_bytes / 1024 / 1024
-                if cpu_usage_4 == "" : cpu_usage_4 = 0
-                if memory_usage_4 == "" : memory_usage_4 = 0
-                if network_usage_4 == "" : network_usage_4 = 0
 
         return F"""
         
@@ -119,7 +93,7 @@ class Ops:
             Last Video: {data[key_data[0]]["last_video_filename"]}
 
             CPU Usage: {float(cpu_usage_0):.2f}%
-            Memory Usage: {float(memory_usage_0) * 1024:.2f} MB
+            Memory Usage: {float(memory_usage_0):.2f} MB
             Storage Usage: {data[key_data[0]]["total_size"]:.2f} MB
             Network Usage: {float(network_usage_0) / 1024:.2f} MB
 
@@ -129,7 +103,7 @@ class Ops:
             Last Video: {data[key_data[1]]["last_video_filename"]}
 
             CPU Usage: {float(cpu_usage_1):.2f}%
-            Memory Usage: {float(memory_usage_1) * 1024:.2f} MB
+            Memory Usage: {float(memory_usage_1):.2f} MB
             Storage Usage: {data[key_data[1]]["total_size"]:.2f} MB
             Network Usage: {float(network_usage_1) / 1024:.2f} MB
 
@@ -139,7 +113,7 @@ class Ops:
             Last Video: {data[key_data[1]]["last_video_filename"]}
 
             CPU Usage: {float(cpu_usage_2):.2f}%
-            Memory Usage: {float(memory_usage_2) * 1024:.2f} MB
+            Memory Usage: {float(memory_usage_2):.2f} MB
             Storage Usage: {data[key_data[2]]["total_size"]:.2f} MB
             Network Usage: {float(network_usage_2) / 1024:.2f} MB
 
@@ -149,7 +123,7 @@ class Ops:
             Last Video: {data[key_data[3]]["last_video_filename"]}
 
             CPU Usage: {float(cpu_usage_3):.2f}%
-            Memory Usage: {float(memory_usage_3) * 1024:.2f} MB
+            Memory Usage: {float(memory_usage_3):.2f} MB
             Storage Usage: {data[key_data[3]]["total_size"]:.2f} MB
             Network Usage: {float(network_usage_3) / 1024:.2f} MB
 
@@ -157,7 +131,7 @@ class Ops:
 
             <b>Server Converter</b>
             CPU Usage: {float(cpu_usage_4):.2f}%
-            Memory Usage: {float(memory_usage_4) * 1024:.2f} MB
+            Memory Usage: {float(memory_usage_4):.2f} MB
             Network Usage: {float(network_usage_4) / 1024:.2f} MB
 
 
