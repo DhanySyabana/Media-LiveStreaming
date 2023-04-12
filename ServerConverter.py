ca8bc76f42a4bea2970c5e8e5641740c3d115f2b
Muhamad Hasbi <moyantcia@gmail.com>
2023-04-12 20:45:51 +0700

    add non-blocking socket server using asyncio

--- ServerConverter.py
+++ ServerConverter.py
@@ -1,6 +1,6 @@
-import socket
 import struct
 import logging
+import asyncio
 from libs.Loggers import Loggers
 from settings.Config import Config
 from libs.HTTPRequest import HTTPRequest
@@ -14,89 +14,96 @@ class ServerConverter:
         self.port = port
         self.buffer_size = buffer_size
         self.max_connection = max_connection
+        self.server = None
         super().__init__()
 
-    def Start(self) -> None:
+    async def start(self):
         logging.info("Starting Server...")
         logging.info(F"Server Host: {self.host}")
         logging.info(F"Server Port: {self.port}")
-        
-        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        s.bind((self.host, self.port))
-        s.listen(self.max_connection)
-        s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 
-        logging.info("Server is Running...")
+        self.server = await asyncio.start_server(
+            self.handle_client, self.host, self.port
+        )
+
+        async with self.server:
+            await self.server.serve_forever()
+
+    async def handle_client(self, client_reader, client_writer):
         try:
             while True:
-                conn, addr = s.accept()
-                with conn:
-                    logging.info(F"Connected by {addr}")
-                    while True:
-                        data_format = struct.Struct('I')
-                        data_length = conn.recv(data_format.size)
+                data_format = struct.Struct('I')
+                data_length = await client_reader.read(data_format.size)
 
-                        if not data_length:
-                            break
-                        logging.info(F"Received request convert video from {addr}")
+                if not data_length:
+                    break
+                logging.info(F"Received request convert video from {client_writer.get_extra_info('peername')}")
 
+                data_length = data_format.unpack(data_length)[0]
+
+                data = bytearray()
+                while len(data) < data_length:
+                    chunk = await client_reader.read(data_length - len(data))
+                    if not chunk:
+                        break
+                    data += chunk
+                
+                data = data.decode("utf-8")
+                data = eval(data)
+
+                response = None
+
+                video_prosessor = VideoProsessor(
+                    environment=data["environment"],
+                    storage_path=data["storage_path"]
+                )
+
+                if data["event"] == "concat":
+                    response = video_prosessor.ConcatTS(
+                        filename=data["filename"],
+                        mode=data["mode"],
+                    )
+                elif data["event"] == "download":
+                    response_http = HTTPRequest(data["method"], data["url"], data["headers"]).Hit()
+                    if response_http.status_code == 200:
+                        file_name = F"{data['sequence']}.ts"
                         
-                        data_length = data_format.unpack(data_length)[0]
-
-                        data = bytearray()
-                        while len(data) < data_length:
-                            chunk = conn.recv(self.buffer_size)
-                            if not chunk:
-                                break
-                            data += chunk
-
-                        data = data.decode("utf-8")
-                        data = eval(data)
-
-                        response = None
-
-                        video_prosessor = VideoProsessor(
-                            environment=data["environment"],
-                            storage_path=data["storage_path"]
+                        response = video_prosessor.WriteFile(
+                            file_name=file_name,
+                            content=response_http.content,
+                            mode="wb",
+                            folder="ts"
                         )
+                        logging.info(F"Success Download Segment: {file_name}")
+                    else:
+                        logging.error(F"Error Download Segment: {response_http.status_code}")
+                    logging.info(F"Succes Download Segment")
+                else:
+                    logging.error(F"Error Event: {data['event']}")
 
-                        if data["event"] == "concat":
-                            response = video_prosessor.ConcatTS(
-                                filename=data["filename"],
-                                mode=data["mode"],
-                            )
-                        elif data["event"] == "download":
-                            response_http = HTTPRequest(data["method"], data["url"], data["headers"]).Hit()
-                            if response_http.status_code == 200:
-                                file_name = F"{data['sequence']}.ts"
-                                
-                                response = video_prosessor.WriteFile(
-                                    file_name=file_name,
-                                    content=response_http.content,
-                                    mode="wb",
-                                    folder="ts"
-                                )
-                                logging.info(F"Success Download Segment: {file_name}")
-                            else:
-                                logging.error(F"Error Download Segment: {response_http.status_code}")
-                            logging.info(F"Succes Download Segment")
-                        else:
-                            logging.error(F"Error Event: {data['event']}")
-                        
-                        response = bytes(str(response), "utf-8")
-                        conn.sendall(response)
+                response = bytes(str(response), "utf-8")
+                client_writer.write(response)
 
-        except KeyboardInterrupt:
-            logging.info("Closing Server Connection...")
-            logging.info("Server Closed")
-            s.close()
+        except ConnectionResetError:
+            pass
 
-if __name__ == "__main__":
+        finally:
+            logging.info(F"Connection closed from {client_writer.get_extra_info('peername')}")
+            client_writer.close()
+
+async def main():
     CONFIG_SERVER = Config().SOCKET_SERVER
-    server_converter = ServerConverter(
+    server = ServerConverter(
         host=CONFIG_SERVER["HOST"],
         port=CONFIG_SERVER["PORT"],
         max_connection=CONFIG_SERVER["MAX_CONNECTION"],
         buffer_size=CONFIG_SERVER["BUFFER_SIZE"]
     )
-    server_converter.Start()
+    await server.start()
+
+if __name__ == "__main__":
+    try:
+        asyncio.run(main())
+    except KeyboardInterrupt:
+        logging.info("Server Stopped")
+        pass
