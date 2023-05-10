e6f98fbedac87dc3599183448f94ce7a7a2d05dc
Muhamad Hasbi <moyantcia@gmail.com>
2023-05-10 23:39:02 +0700

    add headers for inews

--- settings/Config.py
+++ settings/Config.py
@@ -11,8 +11,17 @@ class Config(object):
             UPLOAD_LOCATION = "storage/inews",
             HEADERS = {
                 'origin': 'https://embed.rctiplus.com',
-                'referer': 'https://embed.rctiplus.com',
-                'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/110.0.0.0 Safari/537.36'
+                'referer': 'https://embed.rctiplus.com/',
+                'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/112.0.0.0 Safari/537.36',
+                'sec-ch-ua': 'Chromium";v="112", "Google Chrome";v="112", "Not:A-Brand";v="99',
+                'sec-ch-ua-mobile': '?0',
+                'sec-ch-ua-platform': '"macOS"',
+                'sec-fetch-dest': 'empty',
+                'sec-fetch-mode': 'cors',
+                'sec-fetch-site': 'cross-site',
+                'accept': '*/*',
+                'accept-encoding': 'gzip, deflate, br',
+                'accept-language': 'en-US,en;q=0.9'
             }
         ),
         CNNSTREAMING = dict(
