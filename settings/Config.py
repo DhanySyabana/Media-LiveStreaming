b5b13208a7eb9953e67cec4dc6a058ec3e126454
Indra <indra@kabayan.id>
2023-06-05 11:51:43 +0700

    [fix]kembali pengambilan di website inews

--- settings/Config.py
+++ settings/Config.py
@@ -3,15 +3,26 @@ class Config(object):
     ENGINE = dict(
         INEWSSTREAMING = dict(
             ENVIRONMENT = "dev",
-            URLV1 = "https://www.indihometv.com/livetv/inews",
+            URLV1 = "https://tv.inews.id/live",
             RESOLUTIONV1 = "640x360",
-            PLAYLIST_DIRECTORYV1 = "https://streaming.indihometv.com/joss/134/inews/playlist.m3u8",
-            HOST_DIRECTORYV1 = "https://cdnkbl2.indihometv.com/joss/134/inews",
+            # HOST_DIRECTORYV1 = "https://eng.rctiplus.id", # NEW
+            HOST_DIRECTORYV1 = "https://inews-linier.rctiplus.id", # OLD
+            RESOLUTION = "426x240",
+            HOST_DIRECTORY = "https://midcache.rctiplus.com/live",
             UPLOAD_LOCATION = "storage/inews",
             HEADERS = {
-                'origin': 'https://www.indihometv.com',
-                'referer': 'https://www.indihometv.com/',
-                'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/110.0.0.0 Safari/537.36',
+                'origin': 'https://embed.rctiplus.com',
+                'referer': 'https://embed.rctiplus.com/',
+                'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/112.0.0.0 Safari/537.36',
+                'sec-ch-ua': 'Chromium";v="112", "Google Chrome";v="112", "Not:A-Brand";v="99',
+                'sec-ch-ua-mobile': '?0',
+                'sec-ch-ua-platform': '"macOS"',
+                'sec-fetch-dest': 'empty',
+                'sec-fetch-mode': 'cors',
+                'sec-fetch-site': 'cross-site',
+                'accept': '*/*',
+                'accept-encoding': 'gzip, deflate, br',
+                'accept-language': 'en-US,en;q=0.9'
             }
         ),
         CNNSTREAMING = dict(
