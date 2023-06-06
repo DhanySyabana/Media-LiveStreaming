696437c59cbfa0c6a3998154169870d3982c3864
Indra <indra@kabayan.id>
2023-06-06 12:15:04 +0700

    [fix] notif tele beritasatu

--- Ops.py
+++ Ops.py
@@ -63,8 +63,8 @@ class Ops:
         key_data = list(data.keys())
 
         # Create a figure and GridSpec object
-        fig = plt.figure(figsize=(23, 7))
-        gs = GridSpec(nrows=2, ncols=7, figure=fig)
+        fig = plt.figure(figsize=(23, 5))
+        gs = GridSpec(nrows=3, ncols=5, figure=fig)
 
         # update margin 
         plt.subplots_adjust(left=0.05, bottom=0.05, right=0.95, top=0.95, wspace=0.1, hspace=0.1)
@@ -153,7 +153,7 @@ class Ops:
         ax5.set_xticks([])
         ax5.set_yticks([])
 
-        ax6 = fig.add_subplot(gs[0, 5])
+        ax6 = fig.add_subplot(gs[1, 0])
         color = "black"
         if data[key_data[5]]["total_video_last_hour"] < 6:
             color = "red"
@@ -168,7 +168,7 @@ class Ops:
         ax6.set_xticks([])
         ax6.set_yticks([])
 
-        ax7 = fig.add_subplot(gs[0, 6])
+        ax7 = fig.add_subplot(gs[1, 1])
         color = "black"
         if data[key_data[6]]["total_video_last_hour"] < 6:
             color = "red"
@@ -183,8 +183,23 @@ class Ops:
         ax7.set_xticks([])
         ax7.set_yticks([])
 
-        total_video = [data[key_data[0]]["total_video"], data[key_data[1]]["total_video"], data[key_data[2]]["total_video"], data[key_data[3]]["total_video"], data[key_data[4]]["total_video"], data[key_data[5]]["total_video"], data[key_data[6]]["total_video"]]
-        labels = [key_data[0], key_data[1], key_data[2], key_data[3], key_data[4], key_data[5], key_data[6]]
+        ax8 = fig.add_subplot(gs[1, 2])
+        color = "black"
+        if data[key_data[7]]["total_video_last_hour"] < 6:
+            color = "red"
+            ax8.spines['bottom'].set_color('red')
+            ax8.spines['top'].set_color('red')
+            ax8.spines['right'].set_color('red')
+            ax8.spines['left'].set_color('red')
+        ax8.text(0.5, 0.6, 'CHANNEL', ha='center', va='center', fontsize=10, weight='bold')
+        ax8.text(0.5, 0.5, key_data[6], ha='center', va='center', fontsize=16, weight='bold', color=color)
+        ax8.text(0.5, 0.4, 'LAST VIDEO', ha='center', va='center', fontsize=10, weight='bold')
+        ax8.text(0.5, 0.3, data[key_data[7]]["last_video_filename"], ha='center', va='center', fontsize=10, weight='bold')
+        ax8.set_xticks([])
+        ax8.set_yticks([])
+
+        total_video = [data[key_data[0]]["total_video"], data[key_data[1]]["total_video"], data[key_data[2]]["total_video"], data[key_data[3]]["total_video"], data[key_data[4]]["total_video"], data[key_data[5]]["total_video"], data[key_data[6]]["total_video"], data[key_data[7]]["total_video"]]
+        labels = [key_data[0], key_data[1], key_data[2], key_data[3], key_data[4], key_data[5], key_data[6], key_data[7]]
         colors = plt.cm.Set2(np.linspace(0, 1, len(labels)))
         
         # explode with a bigger value of total_video
@@ -192,7 +207,7 @@ class Ops:
 
         # Add a pie plot to the lower row
         if max(total_video) > 0:
-            ax_total_video = fig.add_subplot(gs[1, 2:4])
+            ax_total_video = fig.add_subplot(gs[2, 2:4])
             # handle autopct, cannot convert float NaN to integer
             autopct = lambda p: '{:.0f}'.format(p * sum(total_video) / 100) if p > 0 else ''
             _, _, autotexts = ax_total_video.pie(total_video, radius=1, colors=colors, autopct=autopct, pctdistance=0.8, startangle=90, explode=explode, wedgeprops = { 'linewidth': 2, "edgecolor" :"k" })
@@ -207,8 +222,8 @@ class Ops:
             ax_total_video.set_title('TOTAL VIDEO', weight='bold', fontsize=16)
 
         # total last video
-        total_video_last_hour = [data[key_data[0]]["total_video_last_hour"], data[key_data[1]]["total_video_last_hour"], data[key_data[2]]["total_video_last_hour"], data[key_data[3]]["total_video_last_hour"], data[key_data[4]]["total_video_last_hour"], data[key_data[5]]["total_video_last_hour"], data[key_data[6]]["total_video_last_hour"]]
-        labels = [key_data[0], key_data[1], key_data[2], key_data[3], key_data[4], key_data[5], key_data[6]]
+        total_video_last_hour = [data[key_data[0]]["total_video_last_hour"], data[key_data[1]]["total_video_last_hour"], data[key_data[2]]["total_video_last_hour"], data[key_data[3]]["total_video_last_hour"], data[key_data[4]]["total_video_last_hour"], data[key_data[5]]["total_video_last_hour"], data[key_data[6]]["total_video_last_hour"], data[key_data[7]]["total_video_last_hour"]]
+        labels = [key_data[0], key_data[1], key_data[2], key_data[3], key_data[4], key_data[5], key_data[6], key_data[7]]
 
         # explode with a bigger value of total_video_last_hour
         explode = [0.1 if i == max(total_video_last_hour) else 0 for i in total_video_last_hour]
@@ -229,8 +244,8 @@ class Ops:
             ax_total_video_1_hour.set_title('TOTAL VIDEO LAST ONE HOUR', weight='bold', fontsize=16)
 
         # total storage
-        total_size = [data[key_data[0]]["total_size"], data[key_data[1]]["total_size"], data[key_data[2]]["total_size"], data[key_data[3]]["total_size"], data[key_data[4]]["total_size"], data[key_data[5]]["total_size"], data[key_data[6]]["total_size"]]
-        labels = [key_data[0], key_data[1], key_data[2], key_data[3], key_data[4], key_data[5], key_data[6]]
+        total_size = [data[key_data[0]]["total_size"], data[key_data[1]]["total_size"], data[key_data[2]]["total_size"], data[key_data[3]]["total_size"], data[key_data[4]]["total_size"], data[key_data[5]]["total_size"], data[key_data[6]]["total_size"], data[key_data[7]]["total_size"]]
+        labels = [key_data[0], key_data[1], key_data[2], key_data[3], key_data[4], key_data[5], key_data[6], key_data[7]]
 
         # explode with a bigger value of total_video_last_hour
         explode = [0.1 if i == min(total_size) else 0 for i in total_size]
