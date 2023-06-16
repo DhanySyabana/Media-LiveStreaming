b6c6f977f339a2d3d9ddd209fb29bcecebae4d19
Indra <indra@kabayan.id>
2023-06-16 14:34:46 +0700

    [fix] durasi tvri

--- Tvri.py
+++ Tvri.py
@@ -102,7 +102,7 @@ class BeritaSatu:
         logging.info(F"Last TS: {last_ts}")
         get_total_files = self.video_prosessor.GetTotalFiles(folder="ts", last_ts=last_ts)
         
-        if get_total_files >= 100:
+        if get_total_files >= 120:
             list_files = self.video_prosessor.ListFiles(folder="ts", last_ts=last_ts)
             return dict(status=True, data_ts=list_files)    
         
