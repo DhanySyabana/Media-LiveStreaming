ba7dc3164f760e005e94df23faa4bcb186c1782e
Indra <indra@kabayan.id>
2023-07-06 14:26:37 +0700

    [fix] host converter

--- settings/Config.py
+++ settings/Config.py
@@ -203,51 +203,51 @@ class Config(object):
     )
     
     ### Server ###
-    # SOCKET_SERVER = dict(
-    #     HOST = "10.10.10.8",
-    #     PORT = 6969,
-    #     MAX_CONNECTION = 4,
-    #     BUFFER_SIZE = 1024
-    # )
-
-    # SOCKET_SERVER_BERITASATU = dict(
-    #     HOST = "10.10.10.13",
-    #     PORT = 6868,
-    #     MAX_CONNECTION = 4,
-    #     BUFFER_SIZE = 1024
-    # )
-
-    # SOCKET_SERVER_IDX = dict(
-    #     HOST = "10.10.10.15",
-    #     PORT = 6767,
-    #     MAX_CONNECTION = 4,
-    #     BUFFER_SIZE = 1024
-    # )
-
-    ### end Server ###
-
-    ### Local ###
-
-    SOCKET_SERVER_IDX = dict(
-        HOST = "127.0.0.1",
-        PORT = 7002,
+    SOCKET_SERVER = dict(
+        HOST = "10.10.10.8",
+        PORT = 6969,
         MAX_CONNECTION = 4,
         BUFFER_SIZE = 1024
     )
 
     SOCKET_SERVER_BERITASATU = dict(
-        HOST = "127.0.0.1",
-        PORT = 7001,
+        HOST = "10.10.10.13",
+        PORT = 6868,
         MAX_CONNECTION = 4,
         BUFFER_SIZE = 1024
     )
 
-    SOCKET_SERVER = dict(
-        HOST = "127.0.0.1",
-        PORT = 7004,
+    SOCKET_SERVER_IDX = dict(
+        HOST = "10.10.10.15",
+        PORT = 6767,
         MAX_CONNECTION = 4,
         BUFFER_SIZE = 1024
     )
+
+    ### end Server ###
+
+    ### Local ###
+
+    # SOCKET_SERVER_IDX = dict(
+    #     HOST = "127.0.0.1",
+    #     PORT = 7002,
+    #     MAX_CONNECTION = 4,
+    #     BUFFER_SIZE = 1024
+    # )
+
+    # SOCKET_SERVER_BERITASATU = dict(
+    #     HOST = "127.0.0.1",
+    #     PORT = 7001,
+    #     MAX_CONNECTION = 4,
+    #     BUFFER_SIZE = 1024
+    # )
+
+    # SOCKET_SERVER = dict(
+    #     HOST = "127.0.0.1",
+    #     PORT = 7004,
+    #     MAX_CONNECTION = 4,
+    #     BUFFER_SIZE = 1024
+    # )
 
     ### end local ###
 
