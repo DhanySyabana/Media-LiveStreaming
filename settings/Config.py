0c18240e7c85be5bfc44bafd981f774a06c31154
Indra <indra@kabayan.id>
2023-07-12 15:44:13 +0700

    [fix] tvone pindah ke vidio

--- settings/Config.py
+++ settings/Config.py
@@ -102,13 +102,35 @@ class Config(object):
                 'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/110.0.0.0 Safari/537.36',
             }
         ),
+        # TVONESTREAMING = dict(
+        #     ENVIRONMENT = "dev",
+        #     URL = "https://www.youtube.com/watch?v=yNKvkPJl-tg&feature=youtu.be",
+        #     QUALITY = "360p",
+        #     UPLOAD_LOCATION = "storage/tvone",
+        #     HEADERS = {
+        #         'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/110.0.0.0 Safari/537.36'
+        #     }
+        # ),
         TVONESTREAMING = dict(
             ENVIRONMENT = "dev",
-            URL = "https://www.youtube.com/watch?v=yNKvkPJl-tg&feature=youtu.be",
-            QUALITY = "360p",
+            URL = "https://www.vidio.com/live/783/tokens",
+            RESOLUTION = "640x360",
+            PLAYLIST = "index.m3u8",
+            HOST_DIRECTORY = "https://etslive-app.vidio.com/live/783/master.m3u8",
             UPLOAD_LOCATION = "storage/tvone",
             HEADERS = {
-                'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/110.0.0.0 Safari/537.36'
+                'origin': 'https://www.vidio.com',
+                'referer': 'https://www.vidio.com/',
+                'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/110.0.0.0 Safari/537.36',
+                'sec-ch-ua': 'Chromium";v="114", "Not.A/Brand";v="8", "Chromium";v="114',
+                'sec-ch-ua-mobile': '?0',
+                'sec-ch-ua-platform': '"Windows"',
+                'sec-fetch-dest': 'empty',
+                'sec-fetch-mode': 'cors',
+                'sec-fetch-site': 'cross-site',
+                'accept': '*/*',
+                'accept-encoding': 'gzip, deflate, br',
+                'accept-language': 'en-US,en;q=0.9'
             }
         ),
         # BERITASATUSTREAMING = dict(
