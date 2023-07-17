7999a6ff620bba192a6d5bf2b9c26dfc5585296f
Indra <indra@kabayan.id>
2023-07-17 11:52:14 +0700

    [fix]update headers di idx

--- settings/Config.py
+++ settings/Config.py
@@ -100,7 +100,16 @@ class Config(object):
             HEADERS = {
                 'origin': 'https://www.indihometv.com',
                 'referer': 'https://www.indihometv.com/',
-                'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/110.0.0.0 Safari/537.36',
+                'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36',
+                'sec-ch-ua': 'Not.A/Brand";v="8", "Chromium";v="114", "Google Chrome";v="114    ',
+                'sec-ch-ua-mobile': '?0',
+                'sec-ch-ua-platform': '"Windows"',
+                'sec-fetch-dest': 'empty',
+                'sec-fetch-mode': 'cors',
+                'sec-fetch-site': 'cross-site',
+                'accept': '*/*',
+                'accept-encoding': 'gzip, deflate, br',
+                'accept-language': 'en-US,en;q=0.9,id;q=0.8'
             }
         ),
         # TVONESTREAMING = dict(
