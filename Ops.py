4be1605a7a6b6b241be5791b0e08becc334bacb3
Indra <indra@kabayan.id>
2023-07-18 16:30:47 +0700

    [fix] notif tele kurang lebar

--- Ops.py
+++ Ops.py
@@ -63,7 +63,7 @@ class Ops:
         key_data = list(data.keys())
 
         # Create a figure and GridSpec object
-        fig = plt.figure(figsize=(23, 10))
+        fig = plt.figure(figsize=(23, 15))
         gs = GridSpec(nrows=4, ncols=5, figure=fig)
 
         # update margin 
@@ -317,11 +317,11 @@ class Ops:
         fig.tight_layout()
         fig.savefig(F"static/{filename}", dpi=300)
 
-        url = F"https://api.telegram.org/bot{self.TOKEN}/sendPhoto?chat_id={self.CHAT_ID}"
-        response = requests.post(url, files={"photo": open(F"static/{filename}", "rb")})
-        if response.ok:
-            os.remove(F"static/{filename}")
-            logging.info("Send chart to telegram")
+        # url = F"https://api.telegram.org/bot{self.TOKEN}/sendPhoto?chat_id={self.CHAT_ID}"
+        # response = requests.post(url, files={"photo": open(F"static/{filename}", "rb")})
+        # if response.ok:
+        #     os.remove(F"static/{filename}")
+        #     logging.info("Send chart to telegram")
 
 
     def execute(self) -> None:
