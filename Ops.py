d2af6e21c6b139059618574cbfbb4bbdc6f70a3b
Indra <indra@kabayan.id>
2023-08-22 16:23:01 +0700

    [fix] send notif

--- Ops.py
+++ Ops.py
@@ -362,11 +362,11 @@ class Ops:
         fig.tight_layout()
         fig.savefig(F"static/{filename}", dpi=300)
 
-        # url = F"https://api.telegram.org/bot{self.TOKEN}/sendPhoto?chat_id={self.CHAT_ID}"
-        # response = requests.post(url, files={"photo": open(F"static/{filename}", "rb")})
-        # if response.ok:
-        #     os.remove(F"static/{filename}")
-        #     logging.info("Send chart to telegram")
+        url = F"https://api.telegram.org/bot{self.TOKEN}/sendPhoto?chat_id={self.CHAT_ID}"
+        response = requests.post(url, files={"photo": open(F"static/{filename}", "rb")})
+        if response.ok:
+            os.remove(F"static/{filename}")
+            logging.info("Send chart to telegram")
 
 
     def execute(self) -> None:
@@ -382,8 +382,6 @@ class Ops:
 
 
     def StartEngine(self):
-        self.execute()
-        exit()
         progress_bar = tqdm.tqdm(total=self.delay_proses, desc="Waiting for next task")
         while self.start_proses:
             try:
