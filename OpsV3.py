01fa6af3eb77ff5ed6bc725cb409a2ca5b5ce0e2
Indra <indra@kabayan.id>
2023-09-11 12:59:31 +0700

    [fix] restart tvone

--- OpsV3.py
+++ OpsV3.py
@@ -217,33 +217,33 @@ class Ops:
                 elif channel == "TVONESTREAMING":
                     logging.info(F"Start : {str(channel)}")
                     # # nama container yang mau di restart
-                    # containers_to_restart = ["engine_server_converter_tvone", "engine_tvone_v1"]
+                    containers_to_restart = ["engine_server_converter_tvone", "engine_tvone_v1"]
 
-                    # for name_containers in containers_to_restart:
-                    #     try:
-                    #         # Membuat koneksi dengan Docker daemon
-                    #         # Menentukan URL Docker API
-                    #         docker_api_url = 'unix://var/run/docker.sock'  # Ganti dengan URL yang sesuai
+                    for name_containers in containers_to_restart:
+                        try:
+                            # Membuat koneksi dengan Docker daemon
+                            # Menentukan URL Docker API
+                            docker_api_url = 'unix://var/run/docker.sock'  # Ganti dengan URL yang sesuai
 
-                    #         # Membuat objek klien Docker dengan URL yang ditentukan
-                    #         client = docker.DockerClient(base_url=docker_api_url)
+                            # Membuat objek klien Docker dengan URL yang ditentukan
+                            client = docker.DockerClient(base_url=docker_api_url)
 
-                    #         # Mengambil objek container berdasarkan nama atau ID
-                    #         container = client.containers.get(name_containers)
+                            # Mengambil objek container berdasarkan nama atau ID
+                            container = client.containers.get(name_containers)
 
-                    #         # Memulai ulang (restart) container
-                    #         container.restart()
+                            # Memulai ulang (restart) container
+                            container.restart()
 
-                    #         logging.info(f"Container '{name_containers}' berhasil di-restart.")
-                    #     except docker.errors.NotFound as e:
-                    #         logging.error(f"Error: Container '{name_containers}' tidak ditemukan.")
-                    #         continue
-                    #     except docker.errors.APIError as e:
-                    #         logging.error(f"Error: Terjadi kesalahan API Docker - {e}")
-                    #         continue
-                    #     except Exception as e:
-                    #         logging.error(f"Error: Terjadi kesalahan yang tidak terduga - {e}")
-                    #         continue
+                            logging.info(f"Container '{name_containers}' berhasil di-restart.")
+                        except docker.errors.NotFound as e:
+                            logging.error(f"Error: Container '{name_containers}' tidak ditemukan.")
+                            continue
+                        except docker.errors.APIError as e:
+                            logging.error(f"Error: Terjadi kesalahan API Docker - {e}")
+                            continue
+                        except Exception as e:
+                            logging.error(f"Error: Terjadi kesalahan yang tidak terduga - {e}")
+                            continue
                 elif channel == "BERITASATUSTREAMING":
                     # nama container yang mau di restart
                     containers_to_restart = ["engine_server_converter_beritasatu", "engine_beritasatu_v1"]
