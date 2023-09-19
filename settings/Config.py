0b230bff8dddae24f75259570289f0a891977384
Indra <indra@kabayan.id>
2023-09-19 16:18:33 +0700

    [add] seatoday

--- settings/Config.py
+++ settings/Config.py
@@ -394,7 +394,28 @@ class Config(object):
                 'accept-encoding': 'gzip, deflate, br',
                 'accept-language': 'en-US,en;q=0.9,id;q=0.8'
             }
-
+        ),
+        SEATODAYSTREAMING = dict(
+            ENVIRONMENT = "dev",
+            URL = "https://www.vidio.com/live/7687/tokens",
+            RESOLUTION = "640x360",
+            PLAYLIST = "B1News_320x240.m3u8",
+            HOST_DIRECTORY = "https://etslive-app.vidio.com/live/7687/master.m3u8",
+            UPLOAD_LOCATION = "storage/seatoday",
+            HEADERS = {
+                'origin': 'https://www.vidio.com',
+                'referer': 'https://www.vidio.com/',
+                'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/110.0.0.0 Safari/537.36',
+                'sec-ch-ua': 'Chromium";v="114", "Not.A/Brand";v="8", "Chromium";v="114',
+                'sec-ch-ua-mobile': '?0',
+                'sec-ch-ua-platform': '"Windows"',
+                'sec-fetch-dest': 'empty',
+                'sec-fetch-mode': 'cors',
+                'sec-fetch-site': 'cross-site',
+                'accept': '*/*',
+                'accept-encoding': 'gzip, deflate, br',
+                'accept-language': 'en-US,en;q=0.9'
+            }
         ),
     )
 
@@ -419,6 +440,7 @@ class Config(object):
             ENGINE_KEYS[11]: ENGINE[ENGINE_KEYS[11]]["UPLOAD_LOCATION"],
             ENGINE_KEYS[12]: ENGINE[ENGINE_KEYS[12]]["UPLOAD_LOCATION"],
             ENGINE_KEYS[13]: ENGINE[ENGINE_KEYS[13]]["UPLOAD_LOCATION"],
+            ENGINE_KEYS[14]: ENGINE[ENGINE_KEYS[13]]["UPLOAD_LOCATION"],
         }
     )
 
@@ -442,6 +464,7 @@ class Config(object):
             ENGINE_KEYS[11]: ENGINE[ENGINE_KEYS[11]]["UPLOAD_LOCATION"],
             ENGINE_KEYS[12]: ENGINE[ENGINE_KEYS[12]]["UPLOAD_LOCATION"],
             ENGINE_KEYS[13]: ENGINE[ENGINE_KEYS[13]]["UPLOAD_LOCATION"],
+            ENGINE_KEYS[14]: ENGINE[ENGINE_KEYS[13]]["UPLOAD_LOCATION"],
         }
     )
     
@@ -465,6 +488,7 @@ class Config(object):
             ENGINE_KEYS[11]: ENGINE[ENGINE_KEYS[11]]["UPLOAD_LOCATION"],
             ENGINE_KEYS[12]: ENGINE[ENGINE_KEYS[12]]["UPLOAD_LOCATION"],
             ENGINE_KEYS[13]: ENGINE[ENGINE_KEYS[13]]["UPLOAD_LOCATION"],
+            ENGINE_KEYS[14]: ENGINE[ENGINE_KEYS[13]]["UPLOAD_LOCATION"],
         }
     )
     
@@ -567,10 +591,24 @@ class Config(object):
         BUFFER_SIZE = 1024
     )
 
+    SOCKET_SERVER_SEATODAY = dict(
+        HOST = "10.10.10.35",
+        PORT = 7474,
+        MAX_CONNECTION = 4,
+        BUFFER_SIZE = 1024
+    )
+
     ### end Server ###
 
     ### Local ###
 
+    # SOCKET_SERVER_SEATODAY = dict(
+    #     HOST = "127.0.0.1",
+    #     PORT = 7014,
+    #     MAX_CONNECTION = 4,
+    #     BUFFER_SIZE = 1024
+    # )
+    
     # SOCKET_SERVER_INEWS = dict(
     #     HOST = "127.0.0.1",
     #     PORT = 7014,
