87515c519a87f5c4a3ce78d8a4de970af4be6f12
Indra <indra@kabayan.id>
2023-10-03 09:39:04 +0700

    [fix] ubah sumber ke vidio

--- settings/Config.py
+++ settings/Config.py
@@ -79,40 +79,18 @@ class Config(object):
         #         'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36'
         #     }
         # ),
-        METROTVSTREAMING = dict(
-            ENVIRONMENT = "dev",
-            URLV1 = "https://www.indihometv.com/livetv/metrotv",
-            RESOLUTIONV1 = "640x360",
-            PLAYLIST_DIRECTORYV1 = "https://streaming.indihometv.com/joss/133/metrotv1080/playlist.m3u8",
-            HOST_DIRECTORYV1 = "https://cdnkbl2.indihometv.com/joss/133/metrotv1080",
-            UPLOAD_LOCATION = "storage/metro",
-            HEADERS = {
-                'origin': 'https://www.indihometv.com',
-                'referer': 'https://www.indihometv.com/',
-                'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36',
-                'sec-ch-ua': 'Not.A/Brand";v="8", "Chromium";v="114", "Google Chrome";v="114    ',
-                'sec-ch-ua-mobile': '?0',
-                'sec-ch-ua-platform': '"Windows"',
-                'sec-fetch-dest': 'empty',
-                'sec-fetch-mode': 'cors',
-                'sec-fetch-site': 'cross-site',
-                'accept': '*/*',
-                'accept-encoding': 'gzip, deflate, br',
-                'accept-language': 'en-US,en;q=0.9,id;q=0.8'
-            }
-        ),
         # METROTVSTREAMING = dict(
         #     ENVIRONMENT = "dev",
-        #     URL = "https://www.vidio.com/live/777/tokens",
-        #     RESOLUTION = "640x360",
-        #     PLAYLIST = "B1News_320x240.m3u8",
-        #     HOST_DIRECTORY = "https://etslive-app.vidio.com/live/777/master.m3u8",
+        #     URLV1 = "https://www.indihometv.com/livetv/metrotv",
+        #     RESOLUTIONV1 = "640x360",
+        #     PLAYLIST_DIRECTORYV1 = "https://streaming.indihometv.com/joss/133/metrotv1080/playlist.m3u8",
+        #     HOST_DIRECTORYV1 = "https://cdnkbl2.indihometv.com/joss/133/metrotv1080",
         #     UPLOAD_LOCATION = "storage/metro",
         #     HEADERS = {
-        #         'origin': 'https://www.vidio.com',
-        #         'referer': 'https://www.vidio.com/',
-        #         'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/110.0.0.0 Safari/537.36',
-        #         'sec-ch-ua': 'Chromium";v="114", "Not.A/Brand";v="8", "Chromium";v="114',
+        #         'origin': 'https://www.indihometv.com',
+        #         'referer': 'https://www.indihometv.com/',
+        #         'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36',
+        #         'sec-ch-ua': 'Not.A/Brand";v="8", "Chromium";v="114", "Google Chrome";v="114    ',
         #         'sec-ch-ua-mobile': '?0',
         #         'sec-ch-ua-platform': '"Windows"',
         #         'sec-fetch-dest': 'empty',
@@ -120,9 +98,31 @@ class Config(object):
         #         'sec-fetch-site': 'cross-site',
         #         'accept': '*/*',
         #         'accept-encoding': 'gzip, deflate, br',
-        #         'accept-language': 'en-US,en;q=0.9'
+        #         'accept-language': 'en-US,en;q=0.9,id;q=0.8'
         #     }
         # ),
+        METROTVSTREAMING = dict(
+            ENVIRONMENT = "dev",
+            URL = "https://www.vidio.com/live/777/tokens",
+            RESOLUTION = "640x360",
+            PLAYLIST = "B1News_320x240.m3u8",
+            HOST_DIRECTORY = "https://etslive-app.vidio.com/live/777/master.m3u8",
+            UPLOAD_LOCATION = "storage/metro",
+            HEADERS = {
+                'origin': 'https://www.vidio.com',
+                'referer': 'https://www.vidio.com/',
+                'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/110.0.0.0 Safari/537.36',
+                'sec-ch-ua': 'Chromium";v="114", "Not.A/Brand";v="8", "Chromium";v="114',
+                'sec-ch-ua-mobile': '?0',
+                'sec-ch-ua-platform': '"Windows"',
+                'sec-fetch-dest': 'empty',
+                'sec-fetch-mode': 'cors',
+                'sec-fetch-site': 'cross-site',
+                'accept': '*/*',
+                'accept-encoding': 'gzip, deflate, br',
+                'accept-language': 'en-US,en;q=0.9'
+            }
+        ),
         CNBCSTREAMING = dict(
             ENVIRONMENT = "dev",
             HOST_DIRECTORY = "https://live.cnbcindonesia.com/livecnbc/smil:cnbctv.smil",
