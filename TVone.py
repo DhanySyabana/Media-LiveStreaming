c2125700d641eda3ffe25552254c9479b0c0988f
indrakabayan <indra@kabayan.id>
2024-08-14 18:09:31 +0700

    fix

--- TVone.py
+++ TVone.py
@@ -179,7 +179,7 @@ class TvOne:
                     data_ts = check_ts["data_ts"]
 
                     if status_ts:
-                        now_filename = F"TVONESTREAMING{datetime.datetime.now().strftime('%m-%d-%H-%M-%S')}"
+                        now_filename = F"TVONESTREAMING_{datetime.datetime.now().strftime('%m-%d-%H-%M-%S')}"
 
                         logging.info("Request to Server Converter - Concat TS")
                         with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
