5d7200c0afb83da181ddd070519964ac7f91d4d6
indrakabayan <indra@kabayan.id>
2024-09-13 18:04:07 +0700

    proxy

--- tesss.py
+++ tesss.py
@@ -6,11 +6,17 @@ youtube_url = "https://www.youtube.com/watch?v=nfgnpM28xDA"
 # Path ke file cookies.txt
 cookies_file = "cookies.txt"
 
+# URL proxy (misalnya: http://username:password@proxyserver:port)
+proxy_url = "socks5://trkcytfh:xtfqu68rlqwr@207.228.7.241:7423"
+
 # Menggunakan cookies untuk login
 session = streamlink.Streamlink()
 
 # Menambahkan cookies ke session Streamlink
 session.set_option("http-cookies", cookies_file)
+
+# Menambahkan proxy ke session Streamlink
+session.set_option("http-proxy", proxy_url)
 
 # Mendapatkan stream yang tersedia
 streams = session.streams(youtube_url)
