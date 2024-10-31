29729215aa851811b1db7cbf382e5e3113b64354
indrakabayan <indra@kabayan.id>
2024-10-31 13:45:04 +0700

    [fix] durasi

--- Sctv.py
+++ Sctv.py
@@ -106,7 +106,7 @@ class BeritaSatu:
         logging.info(F"Last TS: {last_ts}")
         get_total_files = self.video_prosessor.GetTotalFiles(folder="ts", last_ts=last_ts)
         
-        if get_total_files >= 150:
+        if get_total_files >= 80:
             list_files = self.video_prosessor.ListFiles(folder="ts", last_ts=last_ts)
             return dict(status=True, data_ts=list_files)    
         
