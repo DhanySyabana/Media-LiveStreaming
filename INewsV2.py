d418548980826b0a205595a5c10b36e5d57d9d2c
indrakabayan <indra@kabayan.id>
2024-12-27 13:58:26 +0700

    [fix] inews indihome

--- INewsV2.py
+++ INewsV2.py
@@ -4,13 +4,6 @@ import socket
 import struct
 import logging
 import datetime
-import requests
-import json
-
-from selenium import webdriver
-from selenium.webdriver.common.by import By
-from selenium.webdriver.firefox.options import Options as FirefoxOptions
-
 from urllib import parse
 from libs.Loggers import Loggers
 from libs.Selenium import Selenium
@@ -18,153 +11,124 @@ from settings.Config import Config
 from libs.HTTPRequest import HTTPRequest
 from libs.VideoProsessorInews import VideoProsessor
 
-class INewsV1:
+class IDXIndonesiaV1:
 
-    def __init__(
-                self,
-                environment:str,
-                url:str,
-                host_directory:str = None,
-                search_ext:str = ".m3u8",
-                resolution:str = None,
-                upload_location = None,
-                custom_headers:dict = None,
-                converter_host: str = None,
-                converter_port: int = None,
-                buffer_size: int = None,
+    def __init__(self, 
+            environment:str = None,
+            url:str = None,
+            resolution:str = None,
+            upload_location:str = None,
+            headers:dict = None,
+            playlist_directory:str = None,
+            converter_host:str = None,
+            converter_port:int = None,
+            buffer_size:int = None
             ) -> None:
-        self.environment = environment
-        self.url = url
-        self.host_directory = host_directory
-        self.query = None
-        self.search_ext = search_ext
-        self.resolution = resolution
-        self.upload_location = upload_location
-        self.video_duration = 8
-        self.custom_headers = custom_headers
-        self.last_sequence = None
-        self.start_process = True
-        self.segment_status = None
+        self.environment:str = environment
+        self.url:str = url
+        self.resolution:str = resolution
+        self.upload_location:str = upload_location
+        self.headers:dict = headers
+        self.playlist_directory:str = playlist_directory
         self.video_prosessor = VideoProsessor(environment=self.environment, storage_path=self.upload_location)
+        self.start_process = True
+        self.video_duration = 10
+        self.segment_status = None
+        self.last_sequence = None
         self.converter_host = converter_host
         self.converter_port = converter_port
         self.buffer_size = buffer_size
         Loggers()
         super().__init__()
 
-    def GetPathSDI(self, selenium:None) -> str:
-        path_uri = None
-        token = requests.get('https://api.rctiplus.com/api/v1/live-event/4/url?appierid=undefined', headers={'Authorization':'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ2aWQiOjAsInRva2VuIjoiYTQ3MDA0ZTk2ODRkNDBkOSIsInBsIjoid2ViIiwiZGV2aWNlX2lkIjoiMTYwZWZiYmMtMDQ0My0xMWVlLTk0YmUtMDAxNjNlMDM4OGRiIn0.f1zienRzzc0rKo1Mt_BxSqTAaAGxF0Dzmhnx7oJOcbg'})
-        page = json.loads(token.text)
-        path_uri = page['data']['url']
-        # path_uri = path_uri.partition('rcti-sdi.m3u8')[2]
-        auth = path_uri.partition('?auth_key=')[2]
-
-        # print(path_uri)
-        # exit()
-        # print(auth)
-        headers={
-            'accept': 'application/json',
-            'Authorization':'Bearer '+auth,
-            'origin': 'https://www.rctiplus.com',
-            'referer': 'https://www.rctiplus.com/',
-            'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36',
-            'sec-ch-ua': 'Chromium";v="114", "Not.A/Brand";v="8", "Chromium";v="114',
-            'sec-ch-ua-mobile': '?0',
-            'sec-ch-ua-platform': '"Windows"',
-            'sec-fetch-dest': 'empty',
-            'sec-fetch-mode': 'cors',
-            'sec-fetch-site': 'cross-site',
-            'accept': '*/*',
-            'accept-encoding': 'gzip, deflate, br',
-            'accept-language': 'en-US,en;q=0.9'
-            }
-        self.custom_headers = headers
-        # response = requests.get(path_uri, headers=headers )
-        # m3u8_master = m3u8.loads(response.text)
-        # m3u8_data = m3u8_master.data
-        # segments = m3u8_data["playlists"][0]
-
-        # print(segments['stream_info']['resolution'])
-        # exit()
-
-        logging.info(F"Path SDI: {path_uri}")
-        return path_uri
-    
-    def GetPlaylist(self, query_sdi:str) -> str:
+    def GetPlaylistURI(self, url:str) -> str:
         playlist_uri = None
-        url = F"{query_sdi}"
         logging.info(F"URL: {url}")
 
-        response = HTTPRequest("get", url, self.custom_headers).Hit()
+        response = HTTPRequest("get", url, self.headers).Hit()
         if response.status_code == 200:
             m3u8_master = m3u8.loads(response.text)
             playlists = m3u8_master.data["playlists"]
             for playlist in playlists:
                 if playlist["stream_info"]["resolution"] == self.resolution:
-                    # playlist_uri = F"{self.host_directory}/{playlist['uri']}"
-                    playlist_uri = F"{playlist['uri']}"
-                    self.query = playlist['uri'].split('/')[0]
-                    auth = self.query.partition('?auth_key=')[2]
-                    headers={
-                        'accept': 'application/json',
-                        'Authorization':'Bearer '+auth,
-                        'origin': 'https://www.rctiplus.com',
-                        'referer': 'https://www.rctiplus.com/',
-                        'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36',
-                        'sec-ch-ua': 'Chromium";v="114", "Not.A/Brand";v="8", "Chromium";v="114',
-                        'sec-ch-ua-mobile': '?0',
-                        'sec-ch-ua-platform': '"Windows"',
-                        'sec-fetch-dest': 'empty',
-                        'sec-fetch-mode': 'cors',
-                        'sec-fetch-site': 'cross-site',
-                        'accept': '*/*',
-                        'accept-encoding': 'gzip, deflate, br',
-                        'accept-language': 'en-US,en;q=0.9'
-                        }
-                    self.custom_headers = headers
-                    response = HTTPRequest("get", playlist_uri, self.custom_headers).Hit()
+                    playlist_uri = playlist['uri']
                     break
             logging.info("Get Playlist Success")
         else:
             logging.error(F"Error Get Playlist: {response.status_code}")
         
         return playlist_uri
+
+    def GetPlaylistEncrypted(self, selenium:None) -> str:
+        path_uri = None
+        driver = selenium.DriverSelenium()
+        driver.get(self.url)
+
+        while self.start_process:
+            try:
+                for request in driver.requests:
+                    if request.response:
+                        if self.playlist_directory in request.url:
+                            path_uri = request.url
+                            break
+                    if path_uri is not None:
+                        break
+            except KeyError:
+                logging.error("Error: KeyError")
+                self.start_process = False
+                selenium.CloseDriver()
+                break
+            except KeyboardInterrupt:
+                self.start_process = False
+                selenium.CloseDriver()
+                break
+            if path_uri is not None:
+                break
+        
+        logging.info(F"Path Playlist: {path_uri}")
+        return path_uri
+
+    def GetPlaylist(self) -> str:
+        logging.info("Setup Selenium")
+        selenium = Selenium(self.url, {
+            "headless": True,
+        })
+
+        selenium.SeleniumCapabilities()
+        logging.info("Setup Selenium Capabilities")
+
+        selenium.SeleniumOptions()
+        logging.info("Setup Selenium Options")
+
+        logging.info("Find Playlist")
+        playlist = self.GetPlaylistEncrypted(selenium)
+
+        selenium.CloseDriver()
+        logging.info("Close Selenium Driver")
+        
+        return playlist
     
     def GetSegments(self, playlist_uri:str) -> list:
         file_segments = []
 
-        response = HTTPRequest("get", playlist_uri, self.custom_headers).Hit()
+        response = HTTPRequest("get", playlist_uri, self.headers).Hit()
         
         if response.status_code == 200:
             m3u8_master = m3u8.loads(response.text)
             m3u8_data = m3u8_master.data
 
             segments = m3u8_data["segments"]
-            for segment in segments:
-                auth = segment['uri'].partition('?auth_key=')[2]
-                headers={
-                    'accept': 'application/json',
-                    'Authorization':'Bearer '+auth,
-                    'origin': 'https://www.rctiplus.com',
-                    'referer': 'https://www.rctiplus.com/',
-                    'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36',
-                    'sec-ch-ua': 'Chromium";v="114", "Not.A/Brand";v="8", "Chromium";v="114',
-                    'sec-ch-ua-mobile': '?0',
-                    'sec-ch-ua-platform': '"Windows"',
-                    'sec-fetch-dest': 'empty',
-                    'sec-fetch-mode': 'cors',
-                    'sec-fetch-site': 'cross-site',
-                    'accept': '*/*',
-                    'accept-encoding': 'gzip, deflate, br',
-                    'accept-language': 'en-US,en;q=0.9'
-                    }
-                self.custom_headers = headers
-                file_segments.append({
-                    "url": F"{self.host_directory}/{segment['uri']}",
-                    "sequence": int(segment["uri"].split("seq=")[1].split(".ts")[0])
-                })
 
+            # url_host = parse.urlparse(playlist_uri).scheme + "://" + parse.urlparse(playlist_uri).netloc
+
+            for segment in segments:
+                # url  = F"{url_host}/joss/134/idx/{segment['uri']}"
+                file_segments.append({
+                    "url": F"{segment['uri']}",
+                    # "sequence": int(segment["uri"].split("sleng_")[1].split(".ts")[0])
+                    "sequence": int(segment["uri"].split("seq=")[1].split(".ts")[0])
+                    
+                })
             file_segments = file_segments[-5:]
         else:
             file_segments = []
@@ -184,7 +148,7 @@ class INewsV1:
                 "storage_path": self.upload_location,
                 "method": "get",
                 "segments": segments,
-                "headers": self.custom_headers
+                "headers": self.headers
             }
             to_server = str(to_server).encode("utf-8")
             data_format = struct.Struct('I')
@@ -207,37 +171,15 @@ class INewsV1:
             logging.info("Close Connection - Download Segment")
         return None
     
-    def GetToken(self) -> str:
-        logging.info("Setup Selenium")
-        selenium = Selenium(self.url, {
-            "headless": True,
-        })
-
-        selenium.SeleniumCapabilities()
-        logging.info("Setup Selenium Capabilities")
-
-        selenium.SeleniumOptions()
-        logging.info("Setup Selenium Options")
-
-        logging.info("Get Token SDI")
-        token = self.GetPathSDI(selenium)
-
-        # selenium.CloseDriver()
-        logging.info("Close Selenium Driver")
-        
-        return token
-    
-
     def CheckTSFiles(self) -> dict:
         last_ts = F"{self.last_sequence}.ts"
         get_total_files = self.video_prosessor.GetTotalFiles(folder="ts", last_ts=last_ts)
         
-        if get_total_files >= 75:
+        if get_total_files >= 60:
             list_files = self.video_prosessor.ListFiles(folder="ts", last_ts=last_ts)
             return dict(status=True, data_ts=list_files)
         
         return dict(status=False, data_ts=[])
-        
 
     def StartEngine(self) -> None:
         logging.info("Start Engine")
@@ -245,12 +187,12 @@ class INewsV1:
         logging.info("Cleanup TS")
         self.video_prosessor.CleanUPTSFolder()
 
-        logging.info("Get Path SDI")
-        token = self.GetToken()
+        logging.info("Get Playslist Encrypted")
+        playlist_encrypted = self.GetPlaylist()
 
-        logging.info("Get Playlist URI")
-        playlist_uri = self.GetPlaylist(token)
-        
+        logging.info("Get Playlist")
+        playlist_uri = self.GetPlaylistURI(playlist_encrypted)
+
         try:
             while self.start_process:
                 if playlist_uri is not None:
@@ -258,19 +200,11 @@ class INewsV1:
                     segments = self.GetSegments(playlist_uri)
 
                     while len(segments) == 0:
-                        if self.segment_status == 403:
-                            logging.info("Retry Get Token SDI")
-                            token = self.GetToken()
+                        if self.segment_status == 403 or self.segment_status == 410 or self.segment_status == 404:
+                            logging.info("Retry Get Playlist URI - Get Playslist Encrypted")
+                            playlist_encrypted = self.GetPlaylist()
 
-                            logging.info("Retry Get Playlist URI")
-                            playlist_uri = self.GetPlaylist(token)
-
-                        if self.segment_status == 404:
-                            logging.info("Retry Get Token SDI")
-                            token = self.GetToken()
-
-                            logging.info("Retry Get Playlist URI")
-                            playlist_uri = self.GetPlaylist(token)
+                            playlist_uri = self.GetPlaylistURI(playlist_encrypted)
 
                         logging.info("Retry Get Segment URI")
                         segments = self.GetSegments(playlist_uri)
@@ -308,6 +242,7 @@ class INewsV1:
                                 "storage_path": self.upload_location,
                                 "mode": "w",
                                 "filename": now_filename,
+                                "optimize_video" : True,
                             }
                             to_server = str(to_server).encode("utf-8")
                             data_format = struct.Struct('I')
@@ -330,8 +265,10 @@ class INewsV1:
                         self.video_prosessor.CleanUPTSFolder(list_ts=data_ts, metadata=now_filename)
 
                 else:
-                    logging.info("Retry Get Playlist URI")
-                    playlist_uri = self.GetPlaylist(token)
+                    logging.info("Retry Get Playlist URI - Get Playslist Encrypted")
+                    playlist_encrypted = self.GetPlaylist()
+
+                    playlist_uri = self.GetPlaylistURI(playlist_encrypted)
                     time.sleep(self.video_duration)
 
         except KeyboardInterrupt:
@@ -342,19 +279,20 @@ class INewsV1:
             logging.info("Cleanup TS")
             return None
 
+
 if __name__ == "__main__":
     ENGINE_NAME = "INEWSSTREAMING"
     CONFIG = Config()
     ENGINE = CONFIG.ENGINE[ENGINE_NAME]
-    inews = INewsV1(
+    idxindonesia = IDXIndonesiaV1(
         environment=ENGINE["ENVIRONMENT"],
         url=ENGINE["URLV1"],
-        host_directory=ENGINE["HOST_DIRECTORYV1"],
         resolution=ENGINE["RESOLUTIONV1"],
         upload_location=ENGINE["UPLOAD_LOCATION"],
-        custom_headers=ENGINE["HEADERS"],
+        headers=ENGINE["HEADERS"],
+        playlist_directory=ENGINE["PLAYLIST_DIRECTORYV1"],
         converter_host=CONFIG.SOCKET_SERVER_INEWS["HOST"],
         converter_port=CONFIG.SOCKET_SERVER_INEWS["PORT"],
-        buffer_size=CONFIG.SOCKET_SERVER_INEWS["BUFFER_SIZE"],
+        buffer_size=CONFIG.SOCKET_SERVER_INEWS["BUFFER_SIZE"]
     )
-    inews.StartEngine()   
+    idxindonesia.StartEngine()
--- settings/Config.py
+++ settings/Config.py
@@ -1,22 +1,45 @@
 class Config(object):
 
     ENGINE = dict(
+        INEWSSTREAMING = dict(
+            ENVIRONMENT = "dev",
+            URLV1 = "https://www.indihometv.com/livetv/inews",
+            RESOLUTIONV1 = "640x360",
+            # PLAYLIST_DIRECTORYV1 = "https://streaming.indihometv.com/joss/134/inews/playlist.m3u8",
+            PLAYLIST_DIRECTORYV1 = "https://streaming.indihometv.com/atm/hlsv3/inews/playlist.m3u8",
+            HOST_DIRECTORYV1 = "https://cdnkbl2.indihometv.com/joss/134/inews",
+            UPLOAD_LOCATION = "storage/inews",
+            HEADERS = {
+                'origin': 'https://www.indihometv.com',
+                'referer': 'https://www.indihometv.com/',
+                'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36',
+                'sec-ch-ua': 'Not.A/Brand";v="8", "Chromium";v="114", "Google Chrome";v="114    ',
+                'sec-ch-ua-mobile': '?0',
+                'sec-ch-ua-platform': '"Windows"',
+                'sec-fetch-dest': 'empty',
+                'sec-fetch-mode': 'cors',
+                'sec-fetch-site': 'cross-site',
+                'accept': '*/*',
+                'accept-encoding': 'gzip, deflate, br',
+                'accept-language': 'en-US,en;q=0.9,id;q=0.8'
+            }
+        ),
         # INEWSSTREAMING = dict(
         #     ENVIRONMENT = "dev",
-        #     URLV1 = "https://tv.okezone.com/streaming/inews_tv",
+        #     URLV1 = "https://embed.rctiplus.com/live/inews/inewsid",
         #     RESOLUTIONV1 = "640x360",
-        #     # HOST_DIRECTORYV1 = "https://eng.rctiplus.id", # NEW
-        #     HOST_DIRECTORYV1 = "https://mbd-inews.rctiplus.id", # OLD
-        #     RESOLUTION = "426x240",
-        #     HOST_DIRECTORY = "https://midcache.rctiplus.com/live",
+        #     # HOST_DIRECTORYV1 = "https://inewscdn.rctiplus.id", #OLD
+        #     HOST_DIRECTORYV1 = "https://icdn.rctiplus.id", #NEW
+        #     RESOLUTION = "640x360",
+        #     # HOST_DIRECTORY = "https://midcache.rctiplus.com/live",
         #     UPLOAD_LOCATION = "storage/inews",
         #     HEADERS = {
         #         'origin': 'https://embed.rctiplus.com',
         #         'referer': 'https://embed.rctiplus.com/',
-        #         'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/112.0.0.0 Safari/537.36',
-        #         'sec-ch-ua': 'Chromium";v="112", "Google Chrome";v="112", "Not:A-Brand";v="99',
+        #         'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/126.0.0.0 Safari/537.36',
+        #         'sec-ch-ua': 'Chromium";v="114", "Not.A/Brand";v="8", "Chromium";v="114',
         #         'sec-ch-ua-mobile': '?0',
-        #         'sec-ch-ua-platform': '"macOS"',
+        #         'sec-ch-ua-platform': '"Windows"',
         #         'sec-fetch-dest': 'empty',
         #         'sec-fetch-mode': 'cors',
         #         'sec-fetch-site': 'cross-site',
@@ -25,30 +48,6 @@ class Config(object):
         #         'accept-language': 'en-US,en;q=0.9'
         #     }
         # ),
-        INEWSSTREAMING = dict(
-            ENVIRONMENT = "dev",
-            URLV1 = "https://embed.rctiplus.com/live/inews/inewsid",
-            RESOLUTIONV1 = "640x360",
-            # HOST_DIRECTORYV1 = "https://inewscdn.rctiplus.id", #OLD
-            HOST_DIRECTORYV1 = "https://icdn.rctiplus.id", #NEW
-            RESOLUTION = "640x360",
-            # HOST_DIRECTORY = "https://midcache.rctiplus.com/live",
-            UPLOAD_LOCATION = "storage/inews",
-            HEADERS = {
-                'origin': 'https://embed.rctiplus.com',
-                'referer': 'https://embed.rctiplus.com/',
-                'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/126.0.0.0 Safari/537.36',
-                'sec-ch-ua': 'Chromium";v="114", "Not.A/Brand";v="8", "Chromium";v="114',
-                'sec-ch-ua-mobile': '?0',
-                'sec-ch-ua-platform': '"Windows"',
-                'sec-fetch-dest': 'empty',
-                'sec-fetch-mode': 'cors',
-                'sec-fetch-site': 'cross-site',
-                'accept': '*/*',
-                'accept-encoding': 'gzip, deflate, br',
-                'accept-language': 'en-US,en;q=0.9'
-            }
-        ),
         CNNSTREAMING = dict(
             ENVIRONMENT = "dev",
             HOST_DIRECTORY = "https://live.cnnindonesia.com/livecnn/smil:cnntv.smil",
