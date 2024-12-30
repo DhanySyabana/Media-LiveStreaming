1c10616574249417e9e31622981afa175e58d40b
indrakabayan <indra@kabayan.id>
2024-12-30 16:29:48 +0700

    [fix] garuda

---
+++ Garuda.py
@@ -0,0 +1,249 @@
+import m3u8
+import time
+import socket
+import struct
+import logging
+import datetime
+import requests
+from libs.Loggers import Loggers
+from settings.Config import Config
+from libs.HTTPRequest import HTTPRequest
+from libs.VideoProsessorGaruda import VideoProsessor
+
+class GARUDA:
+
+    def __init__(
+            self,
+            environment:str,
+            host_directory:str = None,
+            upload_location:str = None,
+            headers: dict = None,
+            converter_host: str = None,
+            converter_port: int = None,
+            buffer_size: int = None,
+            playlist: str = None,
+            resolution: str = None,
+            url: str = None
+        ) -> None:
+        self.environment = environment
+        self.host_directory = host_directory
+        self.url_segment = None
+        self.upload_location = upload_location
+        self.custom_headers = headers
+        self.start_process = True
+        self.video_duration = 8
+        self.last_sequence = None
+        self.segment_status = None
+        self.converter_host = converter_host
+        self.converter_port = converter_port
+        self.buffer_size = buffer_size
+        self.playlist = playlist
+        self.resolution = resolution
+        self.url = url
+        self.video_prosessor = VideoProsessor(environment=self.environment, storage_path=self.upload_location)
+        Loggers()
+        super().__init__()
+
+    def GetSegment(self, playlist_uri:str) -> list:
+        file_segments = []
+
+        response = HTTPRequest("get", F"{playlist_uri}", self.custom_headers).Hit()
+        
+        # response = requests.get(url =F"{self.host_directory}/{self.playlist}", headers=self.custom_headers, verify=False)
+
+        if response.status_code == 200:
+            m3u8_master = m3u8.loads(response.text)
+            m3u8_data = m3u8_master.data
+            
+
+            segments = m3u8_data["segments"]
+            for segment in segments:
+                file_segments.append({
+                    "url": F"{self.host_directory}/{segment['uri']}",
+                    "sequence": str(segment["uri"]).replace('.ts','')
+                })
+            file_segments = file_segments[-5:]
+        else:
+            file_segments = []
+            self.segment_status = response.status_code
+            logging.error(F"Error Get Segments: {response.status_code}")
+            
+        return file_segments
+
+    def DownloadSegment(self, segments: list) -> None:
+        logging.info("Request to Server Converter - Download Segment")
+        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+            s.connect((self.converter_host, self.converter_port))
+
+            to_server = {
+                "event": "download",
+                "environment": self.environment,
+                "storage_path": self.upload_location,
+                "method": "get",
+                "segments": segments,
+                "headers": self.custom_headers
+            }
+            to_server = str(to_server).encode("utf-8")
+            data_format = struct.Struct('I')
+            data_length = len(to_server)
+            s.sendall(data_format.pack(data_length))
+            offset = 0
+            while offset < data_length:
+                sent_bytes = s.send(to_server[offset:])
+                offset += sent_bytes
+
+            response = s.recv(self.buffer_size)
+            response = eval(response)
+            
+            if response:
+                logging.info(F"Message from Server Converter: {response['message']}")
+                self.last_sequence = response["sequence"]
+                logging.info(F"Last Sequence: {self.last_sequence}")
+
+            s.close()
+            logging.info("Close Connection - Download Segment")
+        return None
+    
+    def CheckTSFiles(self) -> dict:
+        last_ts = F"{self.last_sequence}.ts"
+        logging.info(F"Last TS: {last_ts}")
+        get_total_files = self.video_prosessor.GetTotalFiles(folder="ts", last_ts=last_ts)
+        
+        if get_total_files >= 75:
+            list_files = self.video_prosessor.ListFiles(folder="ts", last_ts=last_ts)
+            return dict(status=True, data_ts=list_files)    
+        
+        return dict(status=False, data_ts=[])
+    
+    def GetPlaylist(self) -> str:
+        playlist_uri = None
+        url = F"{self.url}"
+        logging.info(F"URL: {url}")
+
+        # response = HTTPRequest("get", url, self.custom_headers).Hit()
+        response = requests.get(url, verify=False, headers=self.custom_headers)
+        if response.status_code == 200:
+            m3u8_master = m3u8.loads(response.text)
+            playlists = m3u8_master.data['playlists']
+            for playlist in playlists:
+                if playlist['stream_info']['resolution'] == self.resolution:
+                    playlist_uri = F"{self.host_directory}/{playlist['uri']}"
+                    break
+            logging.info("Get Playlist Success")
+        else:
+            logging.error(F"Error Get Playlist: {response.status_code}")
+        
+        return playlist_uri
+    
+    def StartEngine(self) -> None:
+        logging.info("Start Engine")
+
+        logging.info("Cleanup TS")
+        self.video_prosessor.CleanUPTSFolder()
+
+        logging.info("Get Playlist URI")
+        playlist_uri = self.GetPlaylist()
+        logging.info(F"Playlist URI: {playlist_uri}")
+
+        try:
+            while self.start_process:
+                if playlist_uri is not None:
+                    logging.info("Get Segment URI")
+                    segments = self.GetSegment(playlist_uri)
+
+                    while len(segments) == 0:
+                        if self.segment_status == 403 or self.segment_status == 410 or self.segment_status == 404 or self.segment_status == 503:
+                            logging.info("Retry Get Segment URI")
+                            playlist_uri = self.GetPlaylist()
+                            time.sleep(self.video_duration)
+
+                        logging.info("Retry Get Segment URI")
+                        segments = self.GetSegment(playlist_uri )
+                        time.sleep(self.video_duration)
+
+                    time.sleep(self.video_duration)
+
+                    logging.info("Download segment")
+
+                    try:
+                        self.DownloadSegment(segments)
+                    except ConnectionResetError or ConnectionRefusedError:
+                        while True:
+                            try:
+                                self.DownloadSegment(segments)
+                                break
+                            except ConnectionResetError or ConnectionRefusedError:
+                                logging.error("Retry Download Segment")
+                                time.sleep(self.video_duration)
+                                continue
+                    
+                    check_ts = self.CheckTSFiles()
+                    status_ts = check_ts["status"]
+                    data_ts = check_ts["data_ts"]
+
+                    if status_ts:
+                        now_filename = F"GARUDASTREAMING_{datetime.datetime.now().strftime('%m-%d-%H-%M-%S')}"
+
+                        logging.info("Request to Server Converter - Concat TS")
+                        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+                            s.connect((self.converter_host, self.converter_port))
+
+                            to_server = {
+                                "event": "concat",
+                                "environment": self.environment,
+                                "storage_path": self.upload_location,
+                                "mode": "w",
+                                "filename": now_filename,
+                            }
+
+                            to_server = str(to_server).encode("utf-8")
+                            data_format = struct.Struct('I')
+                            data_length = len(to_server)
+                            s.sendall(data_format.pack(data_length))
+
+                            offset = 0
+                            while offset < data_length:
+                                sent_bytes = s.send(to_server[offset:])
+                                offset += sent_bytes
+
+                            response = s.recv(self.buffer_size)
+                            response = eval(response)
+                            logging.info(F"Message from Server Converter: {response['message']}")
+
+                            s.close()
+                            logging.info("Close Connection - Concat TS")
+
+                        logging.info("Cleanup TS")
+                        self.video_prosessor.CleanUPTSFolder(list_ts=data_ts, metadata=now_filename)
+
+                else:
+                    logging.info("Retry Get Playlist URI")
+                    playlist_uri = self.GetPlaylist()
+                    time.sleep(self.video_duration)
+        except KeyboardInterrupt:
+            self.start_process = False
+            logging.info("Stop Engine")
+
+            self.video_prosessor.CleanUPTSFolder()
+            logging.info("Cleanup TS")
+            return None
+
+
+if __name__ == "__main__":
+    ENGINE_NAME = "GARUDASTREAMING"
+    CONFIG = Config()
+    ENGINE = CONFIG.ENGINE[ENGINE_NAME]
+    GARUDA = GARUDA(
+        environment=ENGINE["ENVIRONMENT"],
+        host_directory=ENGINE["HOST_DIRECTORY"],
+        url=ENGINE["URLV1"],
+        upload_location=ENGINE["UPLOAD_LOCATION"],
+        headers=ENGINE["HEADERS"],
+        converter_host=CONFIG.SOCKET_SERVER_GARUDA["HOST"],
+        converter_port=CONFIG.SOCKET_SERVER_GARUDA["PORT"],
+        buffer_size=CONFIG.SOCKET_SERVER_GARUDA["BUFFER_SIZE"],
+        playlist=ENGINE["PLAYLIST"],
+        resolution=ENGINE["RESOLUTION"],
+    )
+    GARUDA.StartEngine()
+
--- settings/Config.py
+++ settings/Config.py
@@ -433,6 +433,27 @@ class Config(object):
                 'accept-language': 'en-US,en;q=0.9'
             }
         ),
+        GARUDASTREAMING = dict(
+            ENVIRONMENT = "dev",
+            # URLV1 = "https://www.beritasatu.com/livestream", OLD
+            URLV1 = "https://hgmtv.com:19360/garudatvlivestreaming/garudatvlivestreaming.m3u8",
+            RESOLUTION = "640x360",
+            PLAYLIST = "01.m3u8",
+            HOST_DIRECTORY = "https://hgmtv.com:19360/garudatvlivestreaming",
+            UPLOAD_LOCATION = "storage/garuda",
+            HEADERS = {
+                'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36',
+                'sec-ch-ua': 'Chromium";v="114", "Not.A/Brand";v="8", "Chromium";v="114',
+                'sec-ch-ua-mobile': '?0',
+                'sec-ch-ua-platform': '"Windows"',
+                'sec-fetch-dest': 'empty',
+                'sec-fetch-mode': 'cors',
+                'sec-fetch-site': 'cross-site',
+                'accept': '*/*',
+                'accept-encoding': 'gzip, deflate, br',
+                'accept-language': 'en-US,en;q=0.9'
+            }
+        ),
     )
 
     ENGINE_KEYS = list(ENGINE.keys())
@@ -627,11 +648,25 @@ class Config(object):
         MAX_CONNECTION = 4,
         BUFFER_SIZE = 1024
     )
+    
+    SOCKET_SERVER_GARUDA = dict(
+        HOST = "10.10.10.40",
+        PORT = 7171,
+        MAX_CONNECTION = 4,
+        BUFFER_SIZE = 1024
+    )
 
     ### end Server ###
 
     ### Local ###
 
+    # SOCKET_SERVER_GARUDA = dict(
+    #     HOST = "127.0.0.1",
+    #     PORT = 7014,
+    #     MAX_CONNECTION = 4,
+    #     BUFFER_SIZE = 1024
+    # )
+    
     # SOCKET_SERVER_SEATODAY = dict(
     #     HOST = "127.0.0.1",
     #     PORT = 7014,
