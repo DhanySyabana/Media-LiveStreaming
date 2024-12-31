de5be7e4490b873ff5c53042f2eec64cd10d6943
indrakabayan <indra@kabayan.id>
2024-12-31 11:28:32 +0700

    [fix]

--- Garuda.py
+++ Garuda.py
@@ -109,7 +109,7 @@ class GARUDA:
         logging.info(F"Last TS: {last_ts}")
         get_total_files = self.video_prosessor.GetTotalFiles(folder="ts", last_ts=last_ts)
         
-        if get_total_files >= 85:
+        if get_total_files >= 100:
             list_files = self.video_prosessor.ListFiles(folder="ts", last_ts=last_ts)
             return dict(status=True, data_ts=list_files)    
         
