2c35cb730e5572788d404c1b0767418b573cd3a1
indrakabayan <indra@kabayan.id>
2025-02-19 11:28:11 +0700

    [fix] update cookies.txt with new secure cookie values and modify IDXSTREAMING URL

--- settings/Config.py
+++ settings/Config.py
@@ -213,7 +213,7 @@ class Config(object):
         # ),
         IDXSTREAMING = dict(
             ENVIRONMENT = "dev",
-            URL = "https://www.youtube.com/watch?v=Kl2f1BCm6Vk",
+            URL = "https://www.youtube.com/watch?v=4ERY4kr_Lgc",
             QUALITY = "360p",
             UPLOAD_LOCATION = "storage/idx",
             HEADERS = {
