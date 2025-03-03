815fe837ea0246a94e7f26d16f21a47ab80c60ad
indrakabayan <indra@kabayan.id>
2025-03-03 13:42:54 +0700

    fix: update database host in Connector.py

--- settings/Connector.py
+++ settings/Connector.py
@@ -9,7 +9,7 @@ def get_channel_data(channel_name):
     :return: List of rows matching the channel_name.
     """
     db_config = {
-        'host': '192.168.1.100',
+        'host': '10.10.10.1',
         'user': 'root',
         'password': 'Bismillahkabayan123!',
         'database': 'produksi_tv'
