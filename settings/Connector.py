ed59f3d047bffdeb9574953d4ea6e0ba056a7a0c
indrakabayan <indra@kabayan.id>
2025-03-03 13:55:53 +0700

    fix: update database host to host.docker.internal in Connector.py

--- settings/Connector.py
+++ settings/Connector.py
@@ -9,7 +9,7 @@ def get_channel_data(channel_name):
     :return: List of rows matching the channel_name.
     """
     db_config = {
-        'host': '127.0.0.1',
+        'host': 'host.docker.internal',
         'user': 'root',
         'password': 'Bismillahkabayan123!',
         'database': 'produksi_tv'
