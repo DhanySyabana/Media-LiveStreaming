883d283d5c92aef3a4162feb737e760d373fad33
indrakabayan <indra@kabayan.id>
2025-03-11 14:25:59 +0700

    fix: adjust threshold for total files in video processor logic

--- TvriJatim.py
+++ TvriJatim.py
@@ -103,7 +103,7 @@ class BeritaSatu:
         logging.info(F"Last TS: {last_ts}")
         get_total_files = self.video_prosessor.GetTotalFiles(folder="ts", last_ts=last_ts)
         
-        if get_total_files >= 60:
+        if get_total_files > 62:
             list_files = self.video_prosessor.ListFiles(folder="ts", last_ts=last_ts)
             return dict(status=True, data_ts=list_files)    
         
