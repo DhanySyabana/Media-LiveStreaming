dfcda3c259f12a844976ad580f829aa918394647
indrakabayan <indra@kabayan.id>
2025-03-11 12:10:27 +0700

    [feat] add TVRIBALI streaming engine configuration and server converter implementation

--- settings/Config.py
+++ settings/Config.py
@@ -532,6 +532,30 @@ class Config(object):
                 'accept-language': 'en-US,en;q=0.9'
             }
         ),
+        TVRIBALISTREAMING = dict(
+            ENVIRONMENT = "dev",
+            URLV1 = "http://klik.tvri.go.id/",
+            RESOLUTION = "640x360",
+            # RESOLUTION = "480x240", #OLD
+            PLAYLIST = "index.m3u8",
+            HOST_DIRECTORY = "https://ott-balancer.tvri.go.id/live/eds/Bali/hls/Bali.m3u8",
+            HOST_DIRECTORY_TS = "https://ott-balancer.tvri.go.id/live/eds/Bali",
+            UPLOAD_LOCATION = "storage/tvribali",
+            HEADERS = {
+                'origin': 'http://klik.tvri.go.id',
+                'referer': 'http://klik.tvri.go.id/',
+                'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36',
+                'sec-ch-ua': 'Not.A/Brand";v="8", "Chromium";v="114", "Google Chrome";v="114    ',
+                'sec-ch-ua-mobile': '?0',
+                'sec-ch-ua-platform': '"Windows"',
+                'sec-fetch-dest': 'empty',
+                'sec-fetch-mode': 'cors',
+                'sec-fetch-site': 'cross-site',
+                'accept': '*/*',
+                'accept-encoding': 'gzip, deflate, br',
+                'accept-language': 'en-US,en;q=0.9,id;q=0.8'
+            }
+        ),
     )
 
     ENGINE_KEYS = list(ENGINE.keys())
@@ -733,11 +757,25 @@ class Config(object):
         MAX_CONNECTION = 4,
         BUFFER_SIZE = 1024
     )
+    
+    SOCKET_SERVER_TVRIBALI = dict(
+        HOST = "12.12.12.42",
+        PORT = 7172,
+        MAX_CONNECTION = 4,
+        BUFFER_SIZE = 1024
+    )
 
     ### end Server ###
 
     ### Local ###
 
+    # SOCKET_SERVER_TVRIBALI = dict(
+    #     HOST = "127.0.0.1",
+    #     PORT = 7014,
+    #     MAX_CONNECTION = 4,
+    #     BUFFER_SIZE = 1024
+    # )
+    
     # SOCKET_SERVER_SEATODAY = dict(
     #     HOST = "127.0.0.1",
     #     PORT = 7014,
