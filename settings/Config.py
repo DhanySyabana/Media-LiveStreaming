79b73880fc31909a6091e084894552b332243562
indrakabayan <indra@kabayan.id>
2025-03-13 10:07:35 +0700

    fix: update MNCSTREAMING URL and cookies-mnc.txt for improved session data consistency

--- settings/Config.py
+++ settings/Config.py
@@ -476,7 +476,7 @@ class Config(object):
         # ),
         MNCSTREAMING = dict(
             ENVIRONMENT = "dev",
-            URL = "https://www.youtube.com/watch?v=m4-m1PpmGO0",
+            URL = "https://www.youtube.com/watch?v=yOSNrll0Z1A",
             QUALITY = "360p",
             UPLOAD_LOCATION = "storage/mnc",
             HEADERS = {
