95d645ed4341009c6e23f2f2c7edab1f485db63d
indrakabayan <indra@kabayan.id>
2025-03-24 09:33:30 +0700

    fix: update MNCSTREAMING URL to the new video link

--- settings/Config.py
+++ settings/Config.py
@@ -476,7 +476,7 @@ class Config(object):
         # ),
         MNCSTREAMING = dict(
             ENVIRONMENT = "dev",
-            URL = "https://www.youtube.com/watch?v=LR7zri5yWWw",
+            URL = "https://www.youtube.com/watch?v=MXaGBs-FFSU",
             QUALITY = "360p",
             UPLOAD_LOCATION = "storage/mnc",
             HEADERS = {
