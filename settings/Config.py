32dd4b69d3dabdcc6184094ae888493cdf09403c
indrakabayan <indra@kabayan.id>
2025-03-26 16:24:20 +0700

    fix: update MNCSTREAMING URL and add VISITOR_PRIVACY_METADATA to cookies-mnc.txt

--- settings/Config.py
+++ settings/Config.py
@@ -476,7 +476,7 @@ class Config(object):
         # ),
         MNCSTREAMING = dict(
             ENVIRONMENT = "dev",
-            URL = "https://www.youtube.com/watch?v=nbpTsE5ZQJE",
+            URL = "https://www.youtube.com/watch?v=NM_xPJfMds0",
             QUALITY = "360p",
             UPLOAD_LOCATION = "storage/mnc",
             HEADERS = {
