ce56a75e9d850b9dbce366f8755e9fb3cb2935de
indrakabayan <indra@kabayan.id>
2025-03-27 11:10:17 +0700

    fix: increase loop iterations and extend delay duration in Trans7.py

--- Trans7.py
+++ Trans7.py
@@ -1,6 +1,6 @@
 import time
 
 # Looping with a delay between iterations
-for i in range(10):  # Adjust the range as needed
+for i in range(100000000):  # Adjust the range as needed
     print(f"Iteration {i + 1}")
-    time.sleep(1)  # Delay of 1 second per loop
+    time.sleep(5)  # Delay of 1 second per loop
