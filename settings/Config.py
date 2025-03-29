9fe433c7f0411be19590fcfdb1a613afecee1d68
indrakabayan <indra@kabayan.id>
2025-03-29 12:20:07 +0700

    fix: update YouTube URL in Config.py and modify cookies in cookies-mnc.txt

--- settings/Config.py
+++ settings/Config.py
@@ -501,7 +501,7 @@ class Config(object):
         # ),
         MNCSTREAMING = dict(
             ENVIRONMENT = "dev",
-            URL = "https://www.youtube.com/watch?v=NM_xPJfMds0",
+            URL = "https://www.youtube.com/watch?v=y_3BKE9AjH4",
             QUALITY = "360p",
             UPLOAD_LOCATION = "storage/mnc",
             HEADERS = {
