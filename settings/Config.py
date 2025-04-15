0a955eb349251832232704ac2feb44c8a16d7150
indrakabayan <indra@kabayan.id>
2025-04-15 14:29:25 +0700

    fix: update YouTube URL in Config.py and modify cookies.txt for session management

--- settings/Config.py
+++ settings/Config.py
@@ -213,7 +213,7 @@ class Config(object):
         # ),
         IDXSTREAMING = dict(
             ENVIRONMENT = "dev",
-            URL = "https://www.youtube.com/watch?v=ci3i2sExf1E",
+            URL = "https://www.youtube.com/watch?v=QB3oe7Q31F0",
             QUALITY = "360p",
             UPLOAD_LOCATION = "storage/idx",
             HEADERS = {
