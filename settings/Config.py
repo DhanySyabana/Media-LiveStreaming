b6984d15f416fe33c20813f537347c15e9975d43
indrakabayan <indra@kabayan.id>
2025-04-17 19:08:57 +0700

    fix: update MNCSTREAMING URL in Config.py for accuracy

--- settings/Config.py
+++ settings/Config.py
@@ -501,7 +501,7 @@ class Config(object):
         # ),
         MNCSTREAMING = dict(
             ENVIRONMENT = "dev",
-            URL = "https://www.youtube.com/watch?v=CglVsh1qHFk",
+            URL = "https://www.youtube.com/watch?v=B9dEq-lU4yU",
             QUALITY = "360p",
             UPLOAD_LOCATION = "storage/mnc",
             HEADERS = {
