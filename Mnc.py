7db7d2813d33c9809fe33472b03e46686df155d5
indrakabayan <indra@kabayan.id>
2025-07-03 16:00:56 +0700

    fix: increase threshold for total files from 10 to 120 in GetStreamSegment method

--- Mnc.py
+++ Mnc.py
@@ -164,7 +164,7 @@ class MNC:
         last_ts = f"{self.last_sequence}.ts"
         get_total_files = self.video_prosessor.GetTotalFiles(folder="ts", last_ts=last_ts)
         
-        if get_total_files >= 10:
+        if get_total_files >= 120:
             list_files = self.video_prosessor.ListFiles(folder="ts", last_ts=last_ts)
             return dict(status=True, data_ts=list_files)
         
