f00b04ed962260e411247e5ea2427a1ab47facde
indrakabayan <indra@kabayan.id>
2025-07-07 10:08:10 +0700

    fix: refactor MNC class to enhance cookie handling and remove YouTube URL fetching

--- IDXIndonesiaV1.py
+++ IDXIndonesiaV1.py
@@ -12,7 +12,7 @@ from libs.Loggers import Loggers
 from settings.Config import Config
 from libs.HTTPRequest import HTTPRequest
 from libs.VideoProsessorIDX import VideoProsessor
-from libs.Youtube import get_youtube
+# from libs.Youtube import get_youtube
 
 class MNC:
 
@@ -57,6 +57,7 @@ class MNC:
                 if resp.status_code == 200:
                     cookies = resp.text
                     cookies = json.loads(cookies)
+                    self.url = cookies['data']['url']
                     session = streamlink.Streamlink()
                     session.set_option("http-cookies", cookies['data']['cookies'])
                     logging.info("[INFO] Cookies set")
@@ -96,7 +97,7 @@ class MNC:
                 
                 if self.quality not in str(streams):
                     logging.error("No streams found")
-                    self.url = get_youtube(self.id_channel)
+                    # self.url = get_youtube(self.id_channel)
                     break_point += 1
                     if break_point >= 5:    
                         logging.error("Gagal Get URL")
@@ -176,7 +177,7 @@ class MNC:
         logging.info("Cleanup TS")
         self.video_prosessor.CleanUPTSFolder()
         logging.info("Get Live URL Youtube")
-        self.url = get_youtube(self.id_channel)
+        # self.url = get_youtube(self.id_channel)
         self.SetCookies()
 
         try: 
