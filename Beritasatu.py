73171b59bf382beeb7dabcc23b3a8367ed521658
indrakabayan <indra@kabayan.id>
2025-07-11 08:46:27 +0700

    fix: refactor MNC class and update configuration for streaming URL and quality

--- Beritasatu.py
+++ Beritasatu.py
@@ -1,74 +1,132 @@
-import m3u8
 import time
+import m3u8
 import socket
 import struct
 import logging
 import datetime
 import requests
+import streamlink
 import random
+import sys, json
 from libs.Loggers import Loggers
 from settings.Config import Config
 from libs.HTTPRequest import HTTPRequest
 from libs.VideoProsessorBeritsasatu import VideoProsessor
+# from libs.Youtube import get_youtube
 
-class BERITASATU:
+class MNC:
 
     def __init__(
             self,
-            environment:str,
-            host_directory:str = None,
-            upload_location:str = None,
+            environment: str,
+            url: str = None,
+            quality: str = None,
+            upload_location: str = None,
             headers: dict = None,
             converter_host: str = None,
             converter_port: int = None,
             buffer_size: int = None,
-            playlist: str = None,
-            resolution: str = None
+            id_channel: str = None,
+            failure_count: int = 0
         ) -> None:
         self.environment = environment
-        self.host_directory = host_directory
-        self.url_segment = None
+        self.url = url
+        self.quality = quality
+        self.start_process = True
         self.upload_location = upload_location
         self.custom_headers = headers
-        self.start_process = True
-        self.video_duration = 4
+        self.video_duration = 5
         self.last_sequence = None
-        self.segment_status = None
+        self.video_prosessor = VideoProsessor(environment=self.environment, storage_path=self.upload_location)
         self.converter_host = converter_host
         self.converter_port = converter_port
         self.buffer_size = buffer_size
-        self.playlist = playlist
-        self.resolution = resolution
-        self.video_prosessor = VideoProsessor(environment=self.environment, storage_path=self.upload_location)
+        self.id_channel = id_channel
+        self.failure_count = failure_count
         Loggers()
-        super().__init__()
+        self.session = None    
 
-    def GetSegment(self, playlist_uri: list) -> list:
+
+    def SetCookies(self):
+        COOKIE_ENDPOINT = "https://siputri.onlinemonitoring.id/api/cookies/livestreaming?channel=BERITASATUSTREAMING&source=Remote1"
+        max_retry = 5
+        for attempt in range(max_retry):
+            try:
+                resp = requests.get(COOKIE_ENDPOINT, timeout=5, allow_redirects=False)
+                logging.info(f"[DEBUG] Status Code: {resp.status_code}")
+                if resp.status_code == 200:
+                    cookies = resp.text
+                    cookies = json.loads(cookies)
+                    self.url = cookies['data']['url']
+                    session = streamlink.Streamlink()
+                    session.set_option("http-cookies", cookies['data']['cookies'])
+                    logging.info("[INFO] Cookies set")
+                    self.session = session  
+                    return
+                else:
+                    logging.warning(f"[WARN] Error, Status Code: {resp.status_code}, (attempt {attempt+1}/{max_retry})")
+            except requests.RequestException as e:
+                logging.warning(f"[WARN] Gagal total ambil cookies: {e} (attempt {attempt+1}/{max_retry})")
+            time.sleep(5)
+        logging.error("[ERROR] Gagal 5x dalam mengambil cookies. Exiting.")
+        sys.exit(1)
+
+
+    def GetStreamSegment(self) -> list:
         file_segments = []
-        
-        # response = HTTPRequest("get", F"{self.host_directory}/{self.playlist}", self.custom_headers).Hit()
-        response = requests.get(playlist_uri, headers=self.custom_headers, verify=False)
+        break_point = 0
+        try:
+            while True:
+                session = self.session
 
-        if response.status_code == 200:
-            m3u8_master = m3u8.loads(response.text)
-            m3u8_data = m3u8_master.data
+                try:
+                    streams = session.streams(self.url)
+                except Exception as e:
+                    if "LOGIN_REQUIRED" in str(e):
+                        self.login_required_count += 1
+                        logging.warning(f"[WARN] LOGIN_REQUIRED ke-{self.login_required_count}/{self.LOGIN_REQUIRED_LIMIT}")
+                        if self.login_required_count >= self.LOGIN_REQUIRED_LIMIT:
+                            logging.error("[ERROR] LOGIN_REQUIRED terjadi terlalu sering. Keluar.")
+                            exit(1)
+                        logging.info("Ambil ulang cookies karena LOGIN_REQUIRED...")
+                        self.FetchCookies()
+                        continue
+                    else:
+                        logging.error(f"[ERROR] Gagal dapat stream: {e}")
+                        continue
+                
+                if self.quality not in str(streams):
+                    logging.error("No streams found")
+                    # self.url = get_youtube(self.id_channel)
+                    break_point += 1
+                    if break_point >= 5:    
+                        logging.error("Gagal Get URL")
+                        exit()
+                        break
+                    continue
+                
+                stream_url = streams[self.quality]
 
-            segments = m3u8_data["segments"]
-            for segment in segments:
-                print(F"{self.host_directory}/{segment['uri']}")
-                file_segments.append({
-                    "url": F"{self.host_directory}/{segment['uri']}",
-                    "sequence": str(segment["uri"]).replace('.ts','')
-                })
-            file_segments = file_segments[-5:]
-        else:
+                m3u8_obj = m3u8.load(stream_url.args['url'])
+
+                segments = m3u8_obj.segments
+                for segment in segments:
+                    file_segments.append({
+                        "url": segment.uri,
+                        "sequence": int(segment.uri.split("sq/")[1].split("/goap")[0])
+                    })
+                file_segments = file_segments[-5:]
+                break
+        except ValueError as e:
             file_segments = []
-            self.segment_status = response.status_code
-            logging.error(F"Error Get Segments: {response.status_code}")
-            
-        return file_segments
+            logging.error(f"Error Get Stream Segment: {e}")
+        except streamlink.exceptions.PluginError as e:
+            file_segments = []
+            logging.error(f"Error Get Stream Segment: {e}")
 
-    def DownloadSegment(self, segments: list) -> None:
+        return file_segments
+    
+    def RecordStream(self, segments: list) -> None:
         logging.info("Request to Server Converter - Download Segment")
         with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
             s.connect((self.converter_host, self.converter_port))
@@ -81,10 +139,12 @@ class BERITASATU:
                 "segments": segments,
                 "headers": self.custom_headers
             }
+
             to_server = str(to_server).encode("utf-8")
             data_format = struct.Struct('I')
             data_length = len(to_server)
             s.sendall(data_format.pack(data_length))
+
             offset = 0
             while offset < data_length:
                 sent_bytes = s.send(to_server[offset:])
@@ -92,132 +152,96 @@ class BERITASATU:
 
             response = s.recv(self.buffer_size)
             response = eval(response)
-            
             if response:
-                logging.info(F"Message from Server Converter: {response['message']}")
+                logging.info(f"Message from Server Converter: {response['message']}")
                 self.last_sequence = response["sequence"]
-                logging.info(F"Last Sequence: {self.last_sequence}")
+                logging.info(f"Last Sequence: {self.last_sequence}")
 
             s.close()
             logging.info("Close Connection - Download Segment")
         return None
     
     def CheckTSFiles(self) -> dict:
-        last_ts = F"{self.last_sequence}.ts"
-        logging.info(F"Last TS: {last_ts}")
+        last_ts = f"{self.last_sequence}.ts"
         get_total_files = self.video_prosessor.GetTotalFiles(folder="ts", last_ts=last_ts)
         
-        if get_total_files >= 150:
+        if get_total_files >= 120:
             list_files = self.video_prosessor.ListFiles(folder="ts", last_ts=last_ts)
-            return dict(status=True, data_ts=list_files)    
+            return dict(status=True, data_ts=list_files)
         
         return dict(status=False, data_ts=[])
-    
-    def GetPlaylist(self) -> str:
-        playlist_uri = None
-        url = F"{self.host_directory}/{self.playlist}"
-        logging.info(F"URL: {url}")
 
-        # response = HTTPRequest("get", url, self.custom_headers).Hit()
-        response = requests.get(url, headers=self.custom_headers, verify=False)
-        if response.status_code == 200:
-            m3u8_master = m3u8.loads(response.text)
-            # playlists = m3u8_master.data
-            playlists = m3u8_master.data["playlists"]
-            for playlist in playlists:
-                if playlist["stream_info"]["resolution"] == self.resolution:
-                    playlist_uri = F"{self.host_directory}/{playlist['uri']}"
-                    break
-            logging.info("Get Playlist Success")
-        else:
-            logging.error(F"Error Get Playlist: {response.status_code}")
-        
-        return playlist_uri
-    
     def StartEngine(self) -> None:
         logging.info("Start Engine")
 
         logging.info("Cleanup TS")
         self.video_prosessor.CleanUPTSFolder()
+        logging.info("Get Live URL Youtube")
+        # self.url = get_youtube(self.id_channel)
+        self.SetCookies()
 
-        logging.info("Get Playlist URI")
-        playlist_uri = self.GetPlaylist()
-
-        try:
+        try: 
             while self.start_process:
-                if playlist_uri is not None:
-                    logging.info("Get Segment URI")
-                    segments = self.GetSegment(playlist_uri)
+                logging.info("Get Stream Segment")
+                segments = self.GetStreamSegment()
 
-                    while len(segments) == 0:
-                        if self.segment_status == 403 or self.segment_status == 410 or self.segment_status == 404 or self.segment_status == 503:
-                            logging.info("Retry Get Segment URI")
-                            playlist_uri = self.GetPlaylist()
+                while len(segments) == 0:
+                    logging.info("Retrying Stream Segment")
+                    segments = self.GetStreamSegment()
+                    time.sleep(self.video_duration)
+
+                time.sleep(self.video_duration)
+
+                logging.info("Record Stream")
+                try:
+                    self.RecordStream(segments)
+                except ConnectionResetError or ConnectionRefusedError:
+                    while True:
+                        try:
+                            self.RecordStream(segments)
+                            break
+                        except ConnectionResetError or ConnectionRefusedError:
+                            logging.error("Retry Download Segment")
                             time.sleep(self.video_duration)
+                            continue
+                
+                check_ts = self.CheckTSFiles()
+                status_ts = check_ts["status"]
+                data_ts = check_ts["data_ts"]
 
-                        logging.info("Retry Get Segment URI")
-                        segments = self.GetSegment()
-                        time.sleep(self.video_duration)
+                if status_ts:
+                    now_filename = f"BERITASATUSTREAMING_{datetime.datetime.now().strftime('%m-%d-%H-%M-%S')}"
+                    logging.info("Request to Server Converter - Concat TS")
+                    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+                        s.connect((self.converter_host, self.converter_port))
 
-                    time.sleep(self.video_duration)
+                        to_server = {
+                            "event": "concat",
+                            "environment": self.environment,
+                            "storage_path": self.upload_location,
+                            "mode": "w",
+                            "filename": now_filename,
+                        }
+                        to_server = str(to_server).encode("utf-8")
+                        data_format = struct.Struct('I')
+                        data_length = len(to_server)
+                        s.sendall(data_format.pack(data_length))
 
-                    logging.info("Download segment")
+                        offset = 0
+                        while offset < data_length:
+                            sent_bytes = s.send(to_server[offset:])
+                            offset += sent_bytes
 
-                    try:
-                        self.DownloadSegment(segments)
-                    except ConnectionResetError or ConnectionRefusedError:
-                        while True:
-                            try:
-                                self.DownloadSegment(segments)
-                                break
-                            except ConnectionResetError or ConnectionRefusedError:
-                                logging.error("Retry Download Segment")
-                                time.sleep(self.video_duration)
-                                continue
-                    
-                    check_ts = self.CheckTSFiles()
-                    status_ts = check_ts["status"]
-                    data_ts = check_ts["data_ts"]
+                        response = s.recv(self.buffer_size)
+                        response = eval(response)
+                        logging.info(f"Message from Server Converter: {response['message']}")
 
-                    if status_ts:
-                        now_filename = F"BERITASATUSTREAMING_{datetime.datetime.now().strftime('%m-%d-%H-%M-%S')}"
+                        s.close()
+                        logging.info("Close Connection - Concat TS")
 
-                        logging.info("Request to Server Converter - Concat TS")
-                        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-                            s.connect((self.converter_host, self.converter_port))
-
-                            to_server = {
-                                "event": "concat",
-                                "environment": self.environment,
-                                "storage_path": self.upload_location,
-                                "mode": "w",
-                                "filename": now_filename,
-                            }
-
-                            to_server = str(to_server).encode("utf-8")
-                            data_format = struct.Struct('I')
-                            data_length = len(to_server)
-                            s.sendall(data_format.pack(data_length))
-
-                            offset = 0
-                            while offset < data_length:
-                                sent_bytes = s.send(to_server[offset:])
-                                offset += sent_bytes
-
-                            response = s.recv(self.buffer_size)
-                            response = eval(response)
-                            logging.info(F"Message from Server Converter: {response['message']}")
-
-                            s.close()
-                            logging.info("Close Connection - Concat TS")
-
-                        logging.info("Cleanup TS")
-                        self.video_prosessor.CleanUPTSFolder(list_ts=data_ts, metadata=now_filename)
-
-                else:
-                    logging.info("Retry Get Playlist URI")
-                    playlist_uri = self.GetPlaylist()
-                    time.sleep(self.video_duration)
+                    logging.info("Cleanup TS")
+                    self.video_prosessor.CleanUPTSFolder(list_ts=data_ts, metadata=now_filename)
+                
         except KeyboardInterrupt:
             self.start_process = False
             logging.info("Stop Engine")
@@ -226,20 +250,19 @@ class BERITASATU:
             logging.info("Cleanup TS")
             return None
 
-
 if __name__ == "__main__":
     ENGINE_NAME = "BERITASATUSTREAMING"
     CONFIG = Config()
     ENGINE = CONFIG.ENGINE[ENGINE_NAME]
-    BERITASATU = BERITASATU(
+    kompas_tv = MNC(
         environment=ENGINE["ENVIRONMENT"],
-        host_directory=ENGINE["HOST_DIRECTORY"],
+        url=ENGINE["URL"],
+        quality=ENGINE["QUALITY"],
         upload_location=ENGINE["UPLOAD_LOCATION"],
         headers=ENGINE["HEADERS"],
+        id_channel=ENGINE["ID_CHANNEL"],
         converter_host=CONFIG.SOCKET_SERVER_BERITASATU["HOST"],
         converter_port=CONFIG.SOCKET_SERVER_BERITASATU["PORT"],
-        buffer_size=CONFIG.SOCKET_SERVER_BERITASATU["BUFFER_SIZE"],
-        playlist=ENGINE["PLAYLIST"],
-        resolution=ENGINE["RESOLUTION"],
+        buffer_size=CONFIG.SOCKET_SERVER_BERITASATU["BUFFER_SIZE"]
     )
-    BERITASATU.StartEngine()
+    kompas_tv.StartEngine()
