19dc2f776bbc6d1170cd8348868463687acc08dd
indrakabayan <indra@kabayan.id>
2025-07-30 14:18:59 +0700

    fix: update TELE_TOKEN values for consistent configuration across OPS

--- settings/Config.py
+++ settings/Config.py
@@ -789,7 +789,7 @@ class Config(object):
     ENGINE_KEYS = list(ENGINE.keys())
 
     OPS = dict(
-        TELE_TOKEN = "5932299476:AAG4YmekrMEVMHaljj01xOqZX1LuBpjEyBw",
+        TELE_TOKEN = "8213920361:AAEMpyChKoMGb_FHw5QNVI9xdySfjP-yrtk",
         TELE_CHAT_ID = "-1001921508509",
         SEND_TIME = 60 * 30,
         STORAGE_PATH = {
@@ -813,7 +813,7 @@ class Config(object):
 
     ### Notif channel mati ###
     OPSV2 = dict(
-        TELE_TOKEN = "5821659123:AAF_QR8ur3dptVNHSH5MPykGUVgsfRPD82w",
+        TELE_TOKEN = "8213920361:AAEMpyChKoMGb_FHw5QNVI9xdySfjP-yrtk",
         TELE_CHAT_ID = "-1002147735635",
         TELEGRAM_MESSAGE_THREAD_ID = 3042,
         SEND_TIME = 60 * 60,
@@ -838,7 +838,7 @@ class Config(object):
     
     ### restart docker ###
     OPSV3 = dict(
-        TELE_TOKEN = "5988797823:AAEPdu06BRbmL4TWCqGY9-m8gLmr-XRJtH8",
+        TELE_TOKEN = "8213920361:AAEMpyChKoMGb_FHw5QNVI9xdySfjP-yrtk",
         TELE_CHAT_ID = "-897317690",
         SEND_TIME = 60 * 40,
         STORAGE_PATH = {
