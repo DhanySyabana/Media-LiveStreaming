facd9346f77efb854e3082c298f3482069d20821
indrakabayan <indra@kabayan.id>
2026-01-29 09:53:33 +0700

    [update] adjust host directory configurations in Rcti.py and Config.py

--- Rcti.py
+++ Rcti.py
@@ -169,10 +169,10 @@ class INewsV1:
                     # "url": F"{self.host_directory}{self.query}/{segment['uri']}",
                     
                     ## host_directory https://rcti-linier.rctiplus.id
-                    "url": F"{self.host_directory}/{self.query}/{segment['uri']}",
+                    # "url": F"{self.host_directory}/{self.query}/{segment['uri']}",
 
                      ## host_directory https://1s1.rctiplus.id
-                    # "url": F"{self.host_directory}/{segment['uri']}",
+                    "url": F"{self.host_directory}/{segment['uri']}",
                     
                     "sequence": int(segment["uri"].split("seq=")[1].split(".ts")[0])
                 })
--- settings/Config.py
+++ settings/Config.py
@@ -326,10 +326,10 @@ class Config(object):
             ENVIRONMENT = "dev",
             URLV1 = "https://www.rctiplus.com/tv/rcti",
             RESOLUTIONV1 = "640x360",
-            # HOST_DIRECTORYV1 = "https://1s1.rctiplus.id", #NEW 26 Januari 2026
+            HOST_DIRECTORYV1 = "https://1s1.rctiplus.id", #NEW 26 Januari 2026
             # HOST_DIRECTORYV1 = "https://rcdn.rctiplus.id",
             # HOST_DIRECTORYV1 = "https://rcticdn.rctiplus.id", # OLD
-            HOST_DIRECTORYV1 = "https://rcti-linier.rctiplus.id", #NEW
+            # HOST_DIRECTORYV1 = "https://rcti-linier.rctiplus.id", #NEW
             RESOLUTION = "426x240",
             # HOST_DIRECTORY = "https://midcache.rctiplus.com/live",
             UPLOAD_LOCATION = "storage/rcti",
